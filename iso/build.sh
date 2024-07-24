b06f813cf9afe8292820d2061fbd44a893cd868a
Nathan Garside <git@nathangarside.com>
2024-07-24 18:32:20 +0100

    Build customised ISO (#6)

---
+++ iso/build.sh
@@ -0,0 +1,32 @@
+set -euo pipefail
+
+# Create build directory
+
+mkdir --parents bin
+
+# Compile ignition file
+
+podman run \
+	--interactive \
+	--rm \
+	quay.io/coreos/butane:release \
+	--pretty --strict < butane.yml > bin/ignition.json
+
+# Download latest ISO
+
+podman run \
+	--interactive \
+	--rm \
+	--volume ./bin:/opt \
+	quay.io/coreos/coreos-installer:release \
+	download --directory /opt --format iso
+
+mv bin/*.iso bin/original.iso
+mv bin/*.iso.sig bin/original.iso.sig
+
+# Customise ISO
+
+podman run \
+	--interactive \
+	--rm \
+	--volume ./bin:/opt quay.io/coreos/coreos-installer:release iso customize --dest-device /dev/sda --dest-ignition /opt/ignition.json --output /opt/server.iso /opt/original.iso
