cbff368a06452c48d3fff0c5a9cb8b4fa5f4ab21
Nathan Garside <git@nathangarside.com>
2024-07-24 20:22:24 +0100

    Add missing license information

--- iso/build.sh
+++ iso/build.sh
@@ -1,3 +1,7 @@
+#!/usr/bin/env bash
+
+# This is free and unencumbered software released into the public domain.
+
 set -euo pipefail
 
 # Create build directory
--- ostree/setup.sh
+++ ostree/setup.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 # This is free and unencumbered software released into the public domain.
 
 mkdir --parents /etc/images
