c7b57491e3b66e5f16013c620c3144c34faf38bb
Nathan Garside <git@nathangarside.com>
2024-07-25 17:59:05 +0100

    Update build.sh

--- iso/build.sh
+++ iso/build.sh
@@ -1,23 +1,26 @@
 #!/usr/bin/env bash
-
 # This is free and unencumbered software released into the public domain.
 
 set -euo pipefail
 
-# Create build directory
+# Create build directory -------------------------------------------------------
 
 mkdir --parents bin
 
-# Compile ignition file
+# Compile ignition file --------------------------------------------------------
 
 podman run \
 	--interactive \
 	--rm \
 	--volume .:/opt \
 	quay.io/coreos/butane:release \
-	--files-dir /opt --pretty --strict < butane.yml > bin/ignition.json
+		--files-dir /opt \
+		--pretty \
+		--strict \
+		< butane.yml \
+		> bin/ignition.json
 
-# Download latest ISO
+# Download latest ISO ----------------------------------------------------------
 
 podman run \
 	--interactive \
@@ -29,9 +32,14 @@ podman run \
 mv bin/*.iso bin/original.iso
 mv bin/*.iso.sig bin/original.iso.sig
 
-# Customise ISO
+# Customise ISO ----------------------------------------------------------------
 
 podman run \
 	--interactive \
 	--rm \
-	--volume ./bin:/opt quay.io/coreos/coreos-installer:release iso customize --dest-device /dev/sda --dest-ignition /opt/ignition.json --output /opt/server.iso /opt/original.iso
+	--volume ./bin:/opt quay.io/coreos/coreos-installer:release \
+		iso customize \
+		--dest-device /dev/sda \
+		--dest-ignition /opt/ignition.json \
+		--output /opt/server.iso \
+		/opt/original.iso
