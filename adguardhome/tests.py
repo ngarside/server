9554e8abf0652f68dd272d29944d0576c92ad3a8
Nathan Garside <git@nathangarside.com>
2024-12-01 16:21:49 +0000

    Use fixture in unit tests (#31)

--- adguardhome/tests.py
+++ adguardhome/tests.py
@@ -2,18 +2,20 @@
 
 # This is free and unencumbered software released into the public domain.
 
-import random, requests, string, subprocess
+import pytest, random, requests, string, subprocess
 
-def admin_home():
-	try:
-		name = ''.join([random.choice(string.ascii_letters) for _ in range(6)])
-		print()
-		port = random.randrange(1000, 64000)
-		subprocess.run([
-			'docker', 'run', '--detach', '--name', name,
-			'--publish', f'{port}:80', 'ghcr.io/ngarside/adguardhome'
-		])
-		res = requests.get(f'http://localhost:{port}')
-		assert res.status_code == 200
-	finally:
-		subprocess.run(['docker', 'rm', '--force', name])
+@pytest.fixture(autouse=True)
+def fixture():
+	global port
+	name = ''.join([random.choice(string.ascii_letters) for _ in range(6)])
+	port = random.randrange(1000, 64000)
+	subprocess.run([
+		'docker', 'run', '--detach', '--name', name,
+		'--publish', f'{port}:80', 'ghcr.io/ngarside/adguardhome'
+	])
+	yield
+	subprocess.run(['docker', 'rm', '--force', name])
+
+def test_admin_home():
+	res = requests.get(f'http://localhost:{port}')
+	assert res.status_code == 200
