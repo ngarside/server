51448d08bac808750429bd23e27953daf5360591
Nathan Garside <git@nathangarside.com>
2024-12-01 13:58:29 +0000

    Randomise test container name

--- adguardhome/tests.py
+++ adguardhome/tests.py
@@ -2,16 +2,18 @@
 
 # This is free and unencumbered software released into the public domain.
 
-import random, requests, subprocess
+import random, requests, string, subprocess
 
 def test_admin_home():
 	try:
+		name = ''.join([random.choice(string.ascii_letters) for _ in range(6)])
+		print()
 		port = random.randrange(1000, 64000)
 		subprocess.run([
-			'docker', 'run', '--detach', '--name', 'adguardhome',
+			'docker', 'run', '--detach', '--name', name,
 			'--publish', f'{port}:80', 'ghcr.io/ngarside/adguardhome'
 		])
 		res = requests.get(f'http://localhost:{port}')
 		assert res.status_code == 200
 	finally:
-		subprocess.run(['docker', 'rm', '--force', 'adguardhome'])
+		subprocess.run(['docker', 'rm', '--force', name])
