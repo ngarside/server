4f951a7f7ae13e51d25c44215f6ff6b89702772f
Nathan Garside <git@nathangarside.com>
2024-12-01 13:52:48 +0000

    Add admin test file

---
+++ adguardhome/tests/admin.py
@@ -0,0 +1,17 @@
+﻿#!/usr/bin/env python
+
+# This is free and unencumbered software released into the public domain.
+
+import random, requests, subprocess
+
+def test_home():
+	try:
+		port = random.randrange(1000, 64000)
+		subprocess.run([
+			'docker', 'run', '--detach', '--name', 'adguardhome',
+			'--publish', f'{port}:80', 'ghcr.io/ngarside/adguardhome'
+		])
+		res = requests.get(f'http://localhost:{port}')
+		assert res.status_code == 200
+	finally:
+		subprocess.run(['docker', 'rm', '--force', 'adguardhome'])
