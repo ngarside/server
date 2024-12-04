dec3db226e1d64419205abe9c5f55e9266beb3ec
Nathan Garside <git@nathangarside.com>
2024-12-04 20:58:25 +0000

    Enable VM secure boot

--- hyperv/create.ps1
+++ hyperv/create.ps1
@@ -14,9 +14,7 @@ Add-VMHardDiskDrive -VMName Server -Path "F:\Server\Virtual Hard Disks\Data.vhdx
 
 Set-VMProcessor -VMName Server -Count 32
 
-Set-VMFirmware -VMName Server -EnableSecureBoot Off
-
-# Set-VMFirmware -VMName Server -SecureBootTemplate "MicrosoftUEFICertificateAuthority"
+Set-VMFirmware -VMName Server -SecureBootTemplate "MicrosoftUEFICertificateAuthority"
 
 Set-VM -VMName Server -AutomaticStartAction Nothing
 
