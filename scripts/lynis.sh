41ead0134765abb089d96bcd4259b1454ef986d5
Nathan Garside <git@nathangarside.com>
2024-12-06 17:43:07 +0000

    Add script to run Lynis

---
+++ scripts/lynis.sh
@@ -0,0 +1,9 @@
+# This is free and unencumbered software released into the public domain.
+
+curl https://github.com/CISOfy/lynis/archive/refs/tags/3.1.2.tar.gz -LO
+
+gunzip 3.1.2.tar.gz
+
+tar -xvf 3.1.2.tar
+
+cd lynis-3.1.2
