f7fdc67832670ce2f907fb2083d74f5be994412e
Nathan Garside <git@nathangarside.com>
2024-12-06 18:21:23 +0000

    Remove additional packages (#49)

--- ostree/setup.sh
+++ ostree/setup.sh
@@ -25,18 +25,27 @@ echo server > /usr/etc/hostname
 rpm-ostree install hyperv-daemons
 
 rpm-ostree override remove \
+	afterburn-dracut \
+	amd-gpu-firmware \
+	amd-ucode-firmware \
 	coreos-installer \
 	coreos-installer-bootinfra \
+	docker-cli \
 	nano \
 	nano-default-editor \
+	flatpak-session-helper \
 	git-core \
 	ignition \
 	console-login-helper-messages \
 	console-login-helper-messages-profile \
 	console-login-helper-messages-motdgen \
 	console-login-helper-messages-issuegen \
+	google-compute-engine-guest-configs-udev \
 	moby-engine \
+	moby-filesystem \
+	NetworkManager-cloud-setup \
 	rsync \
+	toolbox \
 	nvidia-gpu-firmware
 
 	# doesn't work, don't know why
