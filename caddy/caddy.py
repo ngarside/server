edac705f1213aa429c3860af767e9f548b9c8910
Nathan Garside <git@nathangarside.com>
2024-12-11 14:53:04 +0000

    Add tests for Caddy (#60)

---
+++ caddy/caddy.py
@@ -0,0 +1,23 @@
+﻿#!/usr/bin/env python
+
+# This is free and unencumbered software released into the public domain.
+
+import os, pytest, random, requests, string, subprocess, time
+
+@pytest.fixture(autouse=True, scope='session')
+def fixture():
+	global port
+	name = ''.join([random.choice(string.ascii_letters) for _ in range(6)])
+	port = random.randrange(1000, 64000)
+	dir = os.path.dirname(os.path.realpath(__file__))
+	subprocess.run([
+		'docker', 'run', '--detach', '--name', name, '--publish', f'{port}:80',
+		'--volume', f'{dir}/etc:/etc/caddy:ro', 'ghcr.io/ngarside/caddy'
+	])
+	time.sleep(0.1)
+	yield
+	subprocess.run(['docker', 'rm', '--force', name])
+
+def test_respond():
+	res = requests.get(f'http://localhost:{port}')
+	assert res.status_code == 200
