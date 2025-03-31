f04979342e71d356776862675c183fd0304d02b6
Nathan Garside <git@nathangarside.com>
2025-03-31 21:17:36 +0100

    Refactor tests for AdGuard

--- adguardhome/tests.py
+++ adguardhome/tests.py
@@ -1,29 +1,29 @@
 ﻿#!/usr/bin/env python
-
 # This is free and unencumbered software released into the public domain.
 
-import dns.resolver, os, pytest, random, requests, string, subprocess, time
+import dns.resolver, os, pytest, random, requests, subprocess, time
+
+dir = os.path.dirname(os.path.realpath(__file__))
+name, port_admin, port_dns = random.sample(range(1000, 64000), 3)
 
 @pytest.fixture(autouse=True, scope='session')
 def fixture():
-	global port_admin, port_dns
-	name = ''.join([random.choice(string.ascii_letters) for _ in range(6)])
-	port_admin = random.randrange(1000, 64000)
-	port_dns = random.randrange(1000, 64000)
-	dir = os.path.dirname(os.path.realpath(__file__))
-	subprocess.run([
-		'docker', 'run', '--detach', '--name', name, '--publish', f'{port_admin}:80',
-		'--publish', f'{port_dns}:53', '--volume', f'{dir}/etc:/etc/adguardhome',
-		'ghcr.io/ngarside/adguardhome'
-	])
-	time.sleep(0.1)
+	subprocess.run((
+		f'docker run --detach --name {name} --publish {port_admin}:80 '
+		f'--publish {port_dns}:53 --publish {port_dns}:53/udp '
+		f'--volume {dir}:/etc/adguardhome ghcr.io/ngarside/adguardhome'
+	))
 	yield
-	subprocess.run(['docker', 'rm', '--force', name])
+	subprocess.run(f'docker rm --force {name}')
 
 def test_admin_home():
-	res = requests.get(f'http://localhost:{port_admin}')
-	assert res.status_code == 200
+	response = requests.get(f'http://localhost:{port_admin}', timeout=10)
+	assert response.status_code == 200
 
 def test_dns_tcp():
-	request = dns.message.make_query(qname='example.com', rdtype=dns.rdatatype.ANY)
-	dns.query.tcp(q=request, where='127.0.0.1', port=port_dns, timeout=10)
+	request = dns.message.make_query('example.com', dns.rdatatype.ANY)
+	dns.query.tcp(request, '127.0.0.1', 10, port_dns)
+
+def test_dns_udp():
+	request = dns.message.make_query('example.com', dns.rdatatype.ANY)
+	dns.query.udp(request, '127.0.0.1', 10, port_dns)
