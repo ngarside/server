2272d7300be795637a0d08e3f3ff5b34738e29ce
Nathan Garside <git@nathangarside.com>
2025-04-01 19:57:46 +0100

    Remove sample config file

--- adguardhome/test/tests.py
+++ adguardhome/test/tests.py
@@ -1,18 +1,19 @@
 ﻿#!/usr/bin/env python
 # This is free and unencumbered software released into the public domain.
 
-import dns.resolver, os, pytest, random, requests, subprocess, time
+import dns.resolver, os, pytest, random, requests, subprocess, tempfile
 
-dir = os.path.dirname(os.path.realpath(__file__))
+etc = tempfile.TemporaryDirectory()
 name, port_admin, port_dns = random.sample(range(1000, 64000), 3)
 
 @pytest.fixture(autouse=True, scope='session')
 def fixture():
+	open(os.path.join(etc.name, 'config.yml'), 'w').close()
 	subprocess.run([
 		'docker', 'run', '--detach', '--name', f'{name}', '--publish',
 		f'{port_admin}:80', '--publish', f'{port_dns}:53',
 		'--publish', f'{port_dns}:53/udp', '--volume',
-		f'{dir}:/etc/adguardhome', 'ghcr.io/ngarside/adguardhome',
+		f'{etc.name}:/etc/adguardhome', 'ghcr.io/ngarside/adguardhome',
 	])
 	yield
 	subprocess.run(['docker', 'rm', '--force', f'{name}'])
