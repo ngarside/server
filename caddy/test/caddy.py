94af2b7fb0b348de70c2842f0b1cd046f8c0eef9
Nathan Garside <git@nathangarside.com>
2025-07-13 11:46:38 +0100

    Run tests under podman

--- caddy/test/caddy.py
+++ caddy/test/caddy.py
@@ -14,12 +14,12 @@ def fixture():
 	port = random.randrange(1000, 64000)
 	dir = os.path.dirname(os.path.realpath(__file__))
 	subprocess.run([
-		'docker', 'run', '--detach', '--name', name, '--publish', f'{port}:80',
+		'podman', 'run', '--detach', '--name', name, '--publish', f'{port}:80',
 		'--volume', f'{dir}:/etc/caddy:ro', 'ghcr.io/ngarside/caddy'
 	])
 	time.sleep(0.1)
 	yield
-	subprocess.run(['docker', 'rm', '--force', name])
+	subprocess.run(['podman', 'rm', '--force', name])
 
 def test_respond():
 	res = session.get(f'http://localhost:{port}')
