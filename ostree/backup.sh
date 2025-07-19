6549c5465b402686b6dc89e3c287b5d2aa1d85ae
Nathan Garside <git@nathangarside.com>
2025-07-19 14:12:33 +0100

    Fix command ordering

--- ostree/backup.sh
+++ ostree/backup.sh
@@ -5,14 +5,14 @@ set -euo pipefail
 # Delete dangling snapshot if it wasn't cleaned up correctly
 btrfs subvolume delete /var/data/@backup || true
 
+# Create a new snapshot
+btrfs subvolume snapshot -r /var/data /var/data/@backup
+
 # Initialize restic repository if it doesn't already exist
 restic --insecure-no-password --repo /tmp/restic-repo init || true
 
 # Backup using restic
 restic --insecure-no-password --repo /tmp/restic-repo backup /var/data/@backup
-
-# Create a new snapshot
-btrfs subvolume snapshot -r /var/data /var/data/@backup
 
 # Delete snapshot once backup has completed
 btrfs subvolume delete /var/data/@backup
