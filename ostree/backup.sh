16c05f26bdf553222758f804c80d6d67cb4a5c13
Nathan Garside <git@nathangarside.com>
2025-07-19 14:12:21 +0100

    Delete subvolume after running backup

--- ostree/backup.sh
+++ ostree/backup.sh
@@ -13,3 +13,6 @@ restic --insecure-no-password --repo /tmp/restic-repo backup /var/data/@backup
 
 # Create a new snapshot
 btrfs subvolume snapshot -r /var/data /var/data/@backup
+
+# Delete snapshot once backup has completed
+btrfs subvolume delete /var/data/@backup
