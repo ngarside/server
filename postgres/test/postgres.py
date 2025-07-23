ded3b6a6ecd9c49650f029f0dba54cc736c91519
Nathan Garside <git@nathangarside.com>
2025-07-23 20:06:41 +0100

    Tidy test file

--- postgres/test/postgres.py
+++ postgres/test/postgres.py
@@ -2,34 +2,34 @@
 
 # This is free and unencumbered software released into the public domain.
 
-import os, pytest, random, requests, string, subprocess, time
-import psycopg
-from contextlib import closing
+import contextlib, psycopg, pytest, random, string, subprocess, time
 
 @pytest.fixture(autouse=True, scope='session')
 def fixture():
-	global port
+	global session
 	name = ''.join([random.choice(string.ascii_letters) for _ in range(6)])
 	port = random.randrange(1000, 64000)
-	dir = os.path.dirname(os.path.realpath(__file__))
 	subprocess.run([
-		'docker', 'run', '--env', 'POSTGRES_USER=postgres', '--env', 'POSTGRES_PASSWORD=your_password', '--env', 'POSTGRES_DB=mydb', '--detach', '--name', name, '--publish', f'{port}:5432',
-		'--pull', 'never', '--volume', f'{dir}:/etc/caddy:ro',
-		'ghcr.io/ngarside/postgres'
+		'podman', 'run', '--detach', '--env', 'POSTGRES_DB=postgres', '--env',
+		'POSTGRES_PASSWORD=postgres', '--env', 'POSTGRES_USER=postgres',
+		'--name', name, '--publish', f'{port}:5432', '--pull',
+		'never', 'ghcr.io/ngarside/postgres',
 	])
-	time.sleep(3)
-	global session
-	session = psycopg.connect(
-		host="localhost",
-		port=port,
-		user="postgres",
-		password="your_password"
-	)
+	for _ in range(100):
+		try:
+			session = psycopg.connect(
+				host='localhost',
+				port=port,
+				user='postgres',
+				password='postgres',
+			)
+		except:
+			time.sleep(0.1)
 	yield
-	# subprocess.run(['docker', 'rm', '--force', name])
+	subprocess.run(['podman', 'rm', '--force', name])
 
-def test_respond():
-	with closing(session) as conn:
+def test_select():
+	with contextlib.closing(session) as conn:
 		with conn.cursor() as cur:
-			cur.execute("SELECT 1;")
+			cur.execute('select 1')
 			assert cur.fetchone()[0] == 1
