fd6d6d7919ac2ec37b6ed8db0bcb590f86e31dc1
Nathan Garside <git@nathangarside.com>
2025-07-23 15:34:37 +0100

    Add postgres files

---
+++ postgres/readme.md
@@ -0,0 +1,14 @@
+<!-- This is free and unencumbered software released into the public domain -->
+
+# <p align=center>Postgres
+
+A mirror of the official Postgres container image.
+
+# <p align=center>References
+
+- [hub.docker.com/postgres](https://hub.docker.com/_/postgres)
+- [postgresql.org](https://www.postgresql.org)
+
+# <p align=center>License
+
+This is free and unencumbered software released into the public domain.
---
+++ postgres/test/postgres.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+# This is free and unencumbered software released into the public domain.
+
+import os, pytest, random, requests, string, subprocess, time
+
+session = requests.Session()
+session.mount('http://', requests.adapters.HTTPAdapter(max_retries=10))
+
+@pytest.fixture(autouse=True, scope='session')
+def fixture():
+	global port
+	name = ''.join([random.choice(string.ascii_letters) for _ in range(6)])
+	port = random.randrange(1000, 64000)
+	dir = os.path.dirname(os.path.realpath(__file__))
+	subprocess.run([
+		'podman', 'run', '--detach', '--name', name, '--publish', f'{port}:80',
+		'--pull', 'never', '--volume', f'{dir}:/etc/caddy:ro',
+		'ghcr.io/ngarside/caddy'
+	])
+	time.sleep(0.1)
+	yield
+	subprocess.run(['podman', 'rm', '--force', name])
+
+def test_respond():
+	res = session.get(f'http://localhost:{port}')
+	assert res.status_code == 200
