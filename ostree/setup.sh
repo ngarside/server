eed1a9fec6d680156076a9c3bb7eeecc91554b11
Nathan Garside <git@nathangarside.com>
2025-07-24 16:50:30 +0100

    Use quadlet to schedule Caddy (#141)

--- ostree/setup.sh
+++ ostree/setup.sh
@@ -7,7 +7,6 @@ systemctl enable adguardhome-filesystem
 systemctl enable adguardhome-pull
 systemctl enable adguardhome-resolved
 systemctl enable adguardhome-run
-systemctl enable caddy
 systemctl enable restic.timer
 
 chmod ug=r,o= /usr/etc/restic/restic.{env,sh}
