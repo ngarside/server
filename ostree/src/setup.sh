71531d0ea4f8c0b85e483f306328a1c6f3e6651a
Nathan Garside <git@nathangarside.com>
2025-07-28 18:30:48 +0100

    Split internal network for Penpot

--- ostree/src/setup.sh
+++ ostree/src/setup.sh
@@ -45,6 +45,7 @@ cp /tmp/git/penpot/ops/exporter.container /usr/etc/containers/systemd/users/1001
 cp /tmp/git/penpot/ops/frontend.container /usr/etc/containers/systemd/users/1001/penpot_frontend.container
 cp /tmp/git/penpot/ops/postgres.container /usr/etc/containers/systemd/users/1001/penpot_postgres.container
 cp /tmp/git/penpot/ops/valkey.container /usr/etc/containers/systemd/users/1001/penpot_valkey.container
+cp /tmp/git/penpot/ops/penpot_backend.network /usr/etc/containers/systemd/users/1001/penpot_backend.network
 cp /tmp/git/penpot/ops/penpot_frontend.network /usr/etc/containers/systemd/users/1001/penpot_frontend.network
 
 systemctl enable restic.timer
