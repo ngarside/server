b9ce73e23c8e25ff43a71bfc581cec373bbae529
Nathan Garside <git@nathangarside.com>
2025-07-28 19:51:27 +0100

    Make sourced paths relative to root

--- restic/src/restic.sh
+++ restic/src/restic.sh
@@ -4,7 +4,7 @@
 set -euo pipefail
 
 # Load environment variables
-# shellcheck source=./restic.env
+# shellcheck source=restic/src/restic.env
 source /etc/restic/restic.env
 
 # Delete dangling snapshot if it wasn't cleaned up correctly
