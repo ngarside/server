19d829ebd130574308c06f08cf223d2469ec026e
Nathan Garside <git@nathangarside.com>
2025-07-29 20:38:02 +0100

    Add CI tests for FossFLOW (#184)

---
+++ fossflow/test/fossflow.py
@@ -0,0 +1,24 @@
+﻿#!/usr/bin/env python
+
+# This is free and unencumbered software released into the public domain.
+
+import pytest, random, requests, string, subprocess
+
+session = requests.Session()
+session.mount('http://', requests.adapters.HTTPAdapter(max_retries=10))
+
+@pytest.fixture(autouse=True, scope='session')
+def fixture():
+	global port
+	name = ''.join([random.choice(string.ascii_letters) for _ in range(6)])
+	port = random.randrange(1000, 64000)
+	subprocess.run([
+		'podman', 'run', '--detach', '--name', name, '--publish', f'{port}:80',
+		'--pull', 'never', 'ghcr.io/ngarside/fossflow'
+	])
+	yield
+	subprocess.run(['podman', 'rm', '--force', name])
+
+def test_root():
+	res = session.get(f'http://localhost:{port}')
+	assert res.status_code == 200
