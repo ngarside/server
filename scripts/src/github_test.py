eff96990e3381de635e342cc5c60123f5336f0d2
Nathan Garside <git@nathangarside.com>
2025-07-30 11:55:15 +0100

    Tidy print output

--- scripts/src/github_test.py
+++ scripts/src/github_test.py
@@ -4,35 +4,41 @@
 # Finds and deletes all images from the GitHub container registry which do not
 # have any tags corresponding to branches in this repository.
 
-import pprint, requests, slugify
+import requests, slugify
 
-headers = { 'Authorization': f'Bearer {token}'}
 
 def github_get(url):
+	headers = { 'Authorization': f'Bearer {token}' }
 	response = requests.get(url, headers=headers)
 	response.raise_for_status()
 	return response.json()
 
-packages = github_get('https://api.github.com/users/ngarside/packages?package_type=container')
+print('Initiating purge of GitHub containers')
 
 branches_full = github_get('https://api.github.com/repos/ngarside/server/branches')
 branch_tags = [slugify.sanitize(branch['name']) for branch in branches_full]
-branch_tags = [tag.replace('master', 'latest') for tag in branch_tags]
-pprint.pprint(branch_tags)
 
-for package in packages:
-	print(package['url'])
-	versions = github_get(f'{package['url']}/versions')
+print(f'\nDetected branches:')
+for branch in branch_tags:
+	print(f'\t{branch}')
+
+containers = github_get('https://api.github.com/users/ngarside/packages?package_type=container')
+
+for container in containers:
+	print(f'\nProcessing {container["name"]}:')
+	versions = github_get(f'{container['url']}/versions')
 	for version in versions:
-		print(version['name'])
+		sha = version['name'].split(':')[1][:7]
+		print(f'\t{sha} | ', end='')
 		tags = version['metadata']['container']['tags']
-		print(tags)
 		if 'latest' in tags:
-			print('  keep - master')
+			print('keep | default branch')
 		elif any(tag in branch_tags for tag in tags):
-			print('  keep - active branch')
+			print('keep | active branch')
 		elif len(tags) > 0:
-			print('  branch tag missing - should be deleted')
+			print('del  | missing branch')
 		else:
-			print('  untagged - should be deleted')
+			print('del  | untagged')
 	break
+
+print('\nPurging completed')
