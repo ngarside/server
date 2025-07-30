0041d2a3fb7d9c0019d92a557e032de9bb5fbb2a
Nathan Garside <git@nathangarside.com>
2025-07-30 13:45:50 +0100

    Rename tagify script (#191)

--- scripts/src/tagify.py
+++ scripts/src/tagify.py
@@ -6,14 +6,14 @@
 # - If the branch name is 'master', then the tag will be 'latest'
 # - Otherwise the tag will be the branch name, sanitized using 'slugify.py'
 
-# Run with 'python tag_branch.py'
+# Run with 'python tagify.py'
 
 import os, sys, slugify
 
 if __name__ == '__main__':
 	# Assert CLI argument usage.
 	if len(sys.argv) != 1:
-		print('Usage: python tag_branch.py')
+		print('Usage: python tagify.py')
 		sys.exit(2)
 
 	# Find and transform the branch name.
