d84c0b6183199330e8bd8a5e4a3a523e0cf822b1
Nathan Garside <git@nathangarside.com>
2025-07-30 11:36:16 +0100

    Use new script name

--- scripts/src/github_test.py
+++ scripts/src/github_test.py
@@ -4,7 +4,7 @@
 # Finds and deletes all images from the GitHub container registry which do not
 # have any tags corresponding to branches in this repository.
 
-import pprint, requests, tag_sanitize
+import pprint, requests, slugify
 
 headers = { 'Authorization': f'Bearer {token}'}
 
@@ -16,7 +16,7 @@ def github_get(url):
 packages = github_get('https://api.github.com/users/ngarside/packages?package_type=container')
 
 branches_full = github_get('https://api.github.com/repos/ngarside/server/branches')
-branch_tags = [tag_sanitize.sanitize(branch['name']) for branch in branches_full]
+branch_tags = [slugify.sanitize(branch['name']) for branch in branches_full]
 branch_tags = [tag.replace('master', 'latest') for tag in branch_tags]
 pprint.pprint(branch_tags)
 
