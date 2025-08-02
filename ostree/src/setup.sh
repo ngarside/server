34d9c23d64d9f09e78d757b96916d6db36a80330
Nathan Garside <git@nathangarside.com>
2025-08-02 16:52:09 +0100

    Add outline blueprint

--- ostree/src/setup.sh
+++ ostree/src/setup.sh
@@ -73,6 +73,7 @@ cp /tmp/git/authentik/ops/authentik_valkey.container /usr/etc/containers/systemd
 cp /tmp/git/authentik/ops/authentik_worker.container /usr/etc/containers/systemd/users/1001/authentik_worker.container
 cp /tmp/git/authentik/ops/blueprints_memos.yaml /usr/etc/authentik/blueprints/memos.yaml
 cp /tmp/git/authentik/ops/blueprints_youtrack.yaml /usr/etc/authentik/blueprints/youtrack.yaml
+cp /tmp/git/authentik/ops/blueprints_outline.yaml /usr/etc/authentik/blueprints/outline.yaml
 cp /tmp/git/authentik/ops/logo_memos.png /usr/etc/authentik/media/memos.png
 cp /tmp/git/authentik/ops/logo_youtrack.png /usr/etc/authentik/media/youtrack.png
 cp /tmp/git/authentik/ops/worker.env /usr/etc/authentik/env/worker
