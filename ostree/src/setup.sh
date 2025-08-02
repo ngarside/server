a6a4036d17f9b2b1e05a6f015fb8177a9ebe3ec6
Nathan Garside <git@nathangarside.com>
2025-08-02 12:34:21 +0100

    Add YouTrack application

--- ostree/src/setup.sh
+++ ostree/src/setup.sh
@@ -40,6 +40,9 @@ cp /tmp/git/fossflow/ops/fossflow.network /usr/etc/containers/systemd/users/1001
 cp /tmp/git/memos/ops/memos.container /usr/etc/containers/systemd/users/1001/memos.container
 cp /tmp/git/memos/ops/memos.network /usr/etc/containers/systemd/users/1001/memos.network
 
+cp /tmp/git/memos/ops/youtrack.container /usr/etc/containers/systemd/users/1001/youtrack.container
+cp /tmp/git/memos/ops/youtrack.network /usr/etc/containers/systemd/users/1001/youtrack.network
+
 cp /tmp/git/caddy/ops/caddy.container /usr/etc/containers/systemd/users/1001/caddy.container
 
 cp /tmp/git/penpot/ops/backend.container /usr/etc/containers/systemd/users/1001/penpot_backend.container
@@ -61,7 +64,9 @@ cp /tmp/git/authentik/ops/authentik_server.container /usr/etc/containers/systemd
 cp /tmp/git/authentik/ops/authentik_valkey.container /usr/etc/containers/systemd/users/1001/authentik_valkey.container
 cp /tmp/git/authentik/ops/authentik_worker.container /usr/etc/containers/systemd/users/1001/authentik_worker.container
 cp /tmp/git/authentik/ops/blueprints_memos.yaml /usr/etc/authentik/blueprints/memos.yaml
+cp /tmp/git/authentik/ops/blueprints_youtrack.yaml /usr/etc/authentik/blueprints/youtrack.yaml
 cp /tmp/git/authentik/ops/logo_memos.png /usr/etc/authentik/media/memos.png
+cp /tmp/git/authentik/ops/logo_youtrack.png /usr/etc/authentik/media/youtrack.png
 cp /tmp/git/authentik/ops/worker.env /usr/etc/authentik/env/worker
 
 systemctl enable restic.timer
---
+++ youtrack/readme.md
@@ -0,0 +1,14 @@
+<!-- This is free and unencumbered software released into the public domain -->
+
+# <p align=center>YouTrack
+
+Scripts for running YouTrack via Podman and Systemd.
+
+# <p align=center>References
+
+- [hub.docker.io/jetbrains/youtrack](https://hub.docker.com/r/jetbrains/youtrack)
+- [jetbrains.com/youtrack](https://jetbrains.com/youtrack)
+
+# <p align=center>License
+
+This is free and unencumbered software released into the public domain.
