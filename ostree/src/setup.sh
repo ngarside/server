eca70c68fce8c6d2729970ff71a4152ec94397f1
Nathan Garside <git@nathangarside.com>
2025-08-02 16:49:25 +0100

    Run DB containers

--- ostree/src/setup.sh
+++ ostree/src/setup.sh
@@ -48,6 +48,8 @@ mkdir --parents /usr/etc/outline
 cp /tmp/git/outline/ops/outline.container /usr/etc/containers/systemd/users/1001/outline.container
 cp /tmp/git/outline/ops/outline.env /usr/etc/outline/outline.env
 cp /tmp/git/outline/ops/outline.network /usr/etc/containers/systemd/users/1001/outline.network
+cp /tmp/git/outline/ops/postgres.container /usr/etc/containers/systemd/users/1001/outline_postgres.container
+cp /tmp/git/outline/ops/valkey.container /usr/etc/containers/systemd/users/1001/outline_valkey.container
 
 cp /tmp/git/caddy/ops/caddy.container /usr/etc/containers/systemd/users/1001/caddy.container
 
