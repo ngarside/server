80e87c3c5cd4c3dda9f049abf78ac00c6f3255f4
Nathan Garside <git@nathangarside.com>
2025-08-06 16:40:28 +0100

    Setup cloudflared (#222)

---
+++ cloudflared/readme.md
@@ -0,0 +1,13 @@
+<!-- This is free and unencumbered software released into the public domain -->
+
+# <p align=center>Cloudflared
+
+Scripts for running Cloudflared via Podman and Systemd.
+
+# <p align=center>References
+
+- [github.com/cloudflare/cloudflared](https://github.com/cloudflare/cloudflared)
+
+# <p align=center>License
+
+This is free and unencumbered software released into the public domain.
---
+++ ostree/src/cloudflared.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# This is free and unencumbered software released into the public domain.
+
+set -euo pipefail
+
+mkdir --parents /usr/etc/cloudflared
+cp /tmp/git/cloudflared/ops/cloudflared.env /usr/etc/cloudflared/cloudflared.env
+
+mkdir --parents /usr/etc/containers/systemd/users/1001
+cp /tmp/git/cloudflared/ops/cloudflared.container /usr/etc/containers/systemd/users/1001/cloudflared.container
+cp /tmp/git/cloudflared/ops/cloudflared.network /usr/etc/containers/systemd/users/1001/cloudflared.network
