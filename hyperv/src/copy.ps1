1217b5ff35294a9eeab292831060d597ff3697cb
Nathan Garside <git@nathangarside.com>
2025-08-08 11:14:12 +0100

    Remove copy script

--- hyperv/src/copy.ps1
+++
@@ -1,8 +0,0 @@
-# This is free and unencumbered software released into the public domain.
-
-# Copies files to/from the VM via SCP.
-
-$IP = sudo powershell "(Get-VM -Name Server).NetworkAdapters.IPAddresses[0]"
-
-scp 
-ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null core@$IP 2> $null
