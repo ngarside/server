db78d822385c1d4c210152f32a593615d79b8040
Nathan Garside <git@nathangarside.com>
2025-08-09 18:34:21 +0100

    Add healthcheck for fossflow (#235)

--- fossflow/test/fossflow.py
+++ fossflow/test/fossflow.py
@@ -2,23 +2,26 @@
 
 # This is free and unencumbered software released into the public domain.
 
-import os, pytest, random, requests, string, subprocess
+import os, pytest, random, requests, subprocess
+
+name, port = random.sample(range(1000, 64000), 2)
 
 session = requests.Session()
 session.mount('http://', requests.adapters.HTTPAdapter(max_retries=10))
 
 @pytest.fixture(autouse=True, scope='session')
 def fixture():
-	global port
-	name = ''.join([random.choice(string.ascii_letters) for _ in range(6)])
-	port = random.randrange(1000, 64000)
 	tag = os.getenv('TAG') or 'latest'
 	subprocess.run([
-		'podman', 'run', '--detach', '--name', name, '--publish', f'{port}:80',
+		'podman', 'run', '--detach', '--name', f'{name}', '--publish', f'{port}:80',
 		'--pull', 'never', f'ghcr.io/ngarside/fossflow:{tag}',
 	])
 	yield
-	subprocess.run(['podman', 'rm', '--force', name])
+	subprocess.run(['podman', 'rm', '--force', f'{name}'])
+
+def test_healthcheck():
+	status = subprocess.run(['podman', 'healthcheck', 'run', f'{name}'])
+	assert status.returncode == 0
 
 def test_root():
 	res = session.get(f'http://localhost:{port}')
