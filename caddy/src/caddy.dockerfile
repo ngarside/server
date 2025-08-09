a17b7fc465398c0004d1b078f6ddb1ab25acfb1c
Nathan Garside <git@nathangarside.com>
2025-08-09 11:25:40 +0100

    Add health check for caddy

--- caddy/src/caddy.dockerfile
+++ caddy/src/caddy.dockerfile
@@ -1,20 +1,32 @@
 # This is free and unencumbered software released into the public domain.
 
-FROM docker.io/caddy:builder-alpine@sha256:cc6c40aa7cdea02ef9cb99f3c4e4664ecdb6066ae93ae52ed5288afc511e1241 AS build
+FROM docker.io/caddy:builder-alpine@sha256:cc6c40aa7cdea02ef9cb99f3c4e4664ecdb6066ae93ae52ed5288afc511e1241 AS caddy
 
 RUN xcaddy build --with github.com/caddy-dns/cloudflare
 
 RUN chmod ugo=rx /usr/bin/caddy
 
+FROM docker.io/library/golang:alpine@sha256:c8c5f95d64aa79b6547f3b626eb84b16a7ce18a139e3e9ca19a8c078b85ba80d AS healthcheck
+
+WORKDIR /go
+
+COPY adguardhome/src/healthcheck.go healthcheck.go
+
+RUN go build -ldflags="-w -s" healthcheck.go
+RUN chmod ugo=rx /go/healthcheck
+
 FROM scratch
 
-COPY --from=build /usr/bin/caddy /usr/bin/caddy
+COPY --from=caddy /usr/bin/caddy /usr/bin/caddy
+COPY --from=healthcheck /go/healthcheck /usr/bin/healthcheck
 
 EXPOSE 80
 
 WORKDIR /opt/caddy
 
 ENTRYPOINT ["/usr/bin/caddy"]
+
+HEALTHCHECK CMD ["/usr/bin/healthcheck"]
 
 CMD [ \
 	"run", \
--- caddy/test/caddy.py
+++ caddy/test/caddy.py
@@ -4,24 +4,28 @@
 
 import os, pytest, random, requests, string, subprocess, time
 
+name, port = random.sample(range(1000, 64000), 2)
+
 session = requests.Session()
 session.mount('http://', requests.adapters.HTTPAdapter(max_retries=10))
 
 @pytest.fixture(autouse=True, scope='session')
 def fixture():
 	global port
-	name = ''.join([random.choice(string.ascii_letters) for _ in range(6)])
-	port = random.randrange(1000, 64000)
 	dir = os.path.dirname(os.path.realpath(__file__))
 	tag = os.getenv('TAG') or 'latest'
 	subprocess.run([
-		'podman', 'run', '--detach', '--name', name, '--publish', f'{port}:80',
+		'podman', 'run', '--detach', '--name', f'{name}', '--publish', f'{port}:80',
 		'--pull', 'never', '--volume', f'{dir}:/etc/caddy:ro',
 		f'ghcr.io/ngarside/caddy:{tag}',
 	])
 	time.sleep(0.1)
 	yield
-	subprocess.run(['podman', 'rm', '--force', name])
+	subprocess.run(['podman', 'rm', '--force', f'{name}'])
+
+def test_healthcheck():
+	status = subprocess.run(['podman', 'healthcheck', 'run', f'{name}'])
+	assert status.returncode == 0
 
 def test_respond():
 	res = session.get(f'http://localhost:{port}')
