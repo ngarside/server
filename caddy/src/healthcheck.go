f76c1bd05911ef3550252d41e5a093a26a753170
Nathan Garside <git@nathangarside.com>
2025-08-09 11:58:05 +0100

    Add health check for caddy (#232)
    * Add health check for caddy
    
    * Fix lint errors

---
+++ caddy/src/healthcheck.go
@@ -0,0 +1,24 @@
+﻿// This is free and unencumbered software released into the public domain.
+
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	fmt.Println("Sending healthcheck request...")
+
+	res, err := http.Head("http://0.0.0.0:80/reverse_proxy/upstreams")
+	if err != nil {
+		log.Fatalf("Request errored - %s", err)
+	}
+
+	if res.StatusCode >= 300 {
+		log.Fatalf("Request returned unsuccessful status - %s", res.Status)
+	}
+
+	fmt.Printf("Request passed - %s", res.Status)
+}
