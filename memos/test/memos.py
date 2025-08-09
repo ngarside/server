0ef7e5f9c1f7556c9666638bfbfef4d17efd9733
Nathan Garside <git@nathangarside.com>
2025-08-09 12:15:37 +0100

    Add health check for memos (#233)

--- memos/test/memos.py
+++ memos/test/memos.py
@@ -2,26 +2,29 @@
 
 # This is free and unencumbered software released into the public domain.
 
-import os, pytest, random, requests, string, subprocess, tempfile, time
+import os, pytest, random, requests, subprocess, tempfile, time
+
+name, port = random.sample(range(1000, 64000), 2)
 
 session = requests.Session()
 session.mount('http://', requests.adapters.HTTPAdapter(max_retries=10))
 
 @pytest.fixture(autouse=True, scope='session')
 def fixture():
-	global port
-	name = ''.join([random.choice(string.ascii_letters) for _ in range(6)])
-	port = random.randrange(1000, 64000)
 	dir = tempfile.mkdtemp()
 	tag = os.getenv('TAG') or 'latest'
 	subprocess.run([
-		'podman', 'run', '--detach', '--name', name, '--publish', f'{port}:80',
-		'--pull', 'never', '--volume', f'{dir}:/opt/memos',
+		'podman', 'run', '--detach', '--name', f'{name}', '--publish',
+		f'{port}:80', '--pull', 'never', '--volume', f'{dir}:/opt/memos',
 		f'ghcr.io/ngarside/memos:{tag}',
 	])
 	time.sleep(0.1)
 	yield
-	subprocess.run(['podman', 'rm', '--force', name])
+	subprocess.run(['podman', 'rm', '--force', f'{name}'])
+
+def test_healthcheck():
+	status = subprocess.run(['podman', 'healthcheck', 'run', f'{name}'])
+	assert status.returncode == 0
 
 def test_root():
 	res = session.get(f'http://localhost:{port}')
