dd708bb1baf81c5bf5d41087aaef49063b917a78
Nathan Garside <git@nathangarside.com>
2025-08-09 14:23:04 +0100

    Add network to base image

---
+++ ostree/src/vector.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# This is free and unencumbered software released into the public domain.
+
+set -euo pipefail
+
+mkdir --parents /usr/etc/containers/systemd/users/1001
+cp /tmp/git/vector/ops/vector.network /usr/etc/containers/systemd/users/1001/vector.network
