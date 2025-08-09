60b9a0b9448437dd13a55b0ce3ee421bdb547486
Nathan Garside <git@nathangarside.com>
2025-08-10 00:25:10 +0100

    Don't remove installer

--- ostree/src/rpm.sh
+++ ostree/src/rpm.sh
@@ -8,8 +8,6 @@ rpm-ostree install hyperv-daemons restic
 rpm-ostree override remove \
 	amd-gpu-firmware \
 	amd-ucode-firmware \
-	coreos-installer \
-	coreos-installer-bootinfra \
 	docker-cli \
 	nano \
 	nano-default-editor \
