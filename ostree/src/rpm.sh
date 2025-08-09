6e1629d403860aa81189b2416e3b7f04c38ef90e
Nathan Garside <git@nathangarside.com>
2025-08-10 00:34:52 +0100

    Don't remove git

--- ostree/src/rpm.sh
+++ ostree/src/rpm.sh
@@ -11,7 +11,6 @@ rpm-ostree override remove \
 	nano \
 	nano-default-editor \
 	flatpak-session-helper \
-	git-core \
 	ignition \
 	console-login-helper-messages \
 	console-login-helper-messages-profile \
