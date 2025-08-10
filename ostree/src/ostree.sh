86be403fd6e3e10750490468781e0258bde8d5ce
Nathan Garside <git@nathangarside.com>
2025-08-10 10:20:32 +0100

    Fix sudoers folder not existing

--- ostree/src/ostree.sh
+++ ostree/src/ostree.sh
@@ -11,5 +11,6 @@ cp /tmp/git/ostree/ops/sysctl.conf /usr/etc/sysctl.d/0-server.conf
 
 cp /tmp/git/ostree/ops/subuid.txt /usr/etc/subuid
 
+mkdir --parents /usr/etc/sudoers.d
 cp /tmp/git/ostree/ops/sudoers.conf /usr/etc/sudoers.d/server
 chmod 0440 /usr/etc/sudoers.d/server
