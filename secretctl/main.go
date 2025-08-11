f806f032fbc0935ec7baa8796b652621dbcdeacf
Nathan Garside <git@nathangarside.com>
2025-08-11 14:29:00 +0100

    Validate individual secrets

--- secretctl/main.go
+++ secretctl/main.go
@@ -2,6 +2,7 @@
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"os"
 
@@ -25,6 +26,26 @@ type Secret struct {
 	Value  string
 }
 
+func validate(secret Secret) error {
+	if secret.Driver == "" {
+		return fmt.Errorf("driver is required")
+	}
+
+	if secret.Key == "" {
+		return fmt.Errorf("key is required")
+	}
+
+	if secret.User == "" {
+		return fmt.Errorf("user is required")
+	}
+
+	if secret.Value == "" {
+		return fmt.Errorf("value is required")
+	}
+
+	return nil
+}
+
 func main() {
 	// Return early unless the program is run with a single "cat" command
 	if len(os.Args) != 2 || os.Args[1] != "cat" {
@@ -38,11 +59,18 @@ func main() {
 		log.Fatal(err)
 	}
 
+	for _, secret := range file.Secrets {
+		log.Println(secret.User)
+		err = validate(secret)
+		if err != nil {
+			log.Fatal("SECRET err")
+		}
+	}
+
 	jsonData, err := json.MarshalIndent(file.Secrets, "", "  ")
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	log.Println(string(jsonData))
-
 }
