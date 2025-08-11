c1c03fe18fe893ddec72e8edca80d54179185bc0
Nathan Garside <git@nathangarside.com>
2025-08-11 14:06:58 +0100

    Load secrets from file

---
+++ secretctl/go.mod
@@ -0,0 +1,5 @@
+module secretctl
+
+go 1.24
+
+require github.com/BurntSushi/toml v1.5.0 // indirect
---
+++ secretctl/main.go
@@ -0,0 +1,35 @@
+﻿package main
+
+import (
+	"encoding/json"
+	"log"
+
+	"github.com/BurntSushi/toml"
+)
+
+type SecretFile struct {
+	Secrets []Secret
+}
+
+type Secret struct {
+	Driver string
+	Key    string
+	User   string
+	Value  string
+}
+
+func main() {
+	var file SecretFile
+	_, err := toml.DecodeFile("C:\\Users\\Nathan\\Projects\\Server\\secretctl\\example.toml", &file)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	jsonData, err := json.MarshalIndent(file.Secrets, "", "  ")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println(string(jsonData))
+
+}
