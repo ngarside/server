fe0710e72fc9d730f9d5c5fd0c045b065d3e0996
Nathan Garside <git@nathangarside.com>
2025-08-15 08:18:35 +0100

    Fix creating ostree folder

--- ostree/src/vector.sh
+++ ostree/src/vector.sh
@@ -7,6 +7,6 @@ mkdir --parents /usr/etc/containers/systemd/users/1001
 cp /tmp/git/vector/ops/vector.container /usr/etc/containers/systemd/users/1001/vector.container
 cp /tmp/git/vector/ops/vector.network /usr/etc/containers/systemd/users/1001/vector.network
 
-mkdir --parents /usr/etc/vector
+mkdir --parents /usr/etc/vector/unprivileged
 cp /tmp/git/vector/ops/unprivileged_host.toml /usr/etc/vector/unprivileged/host.toml
 cp /tmp/git/vector/ops/unprivileged_vector.toml /usr/etc/vector/unprivileged/vector.toml
