b1ca5dc1171718c59b75bb857b8d019bd025e127
Nathan Garside <git@nathangarside.com>
2025-08-15 08:41:21 +0100

    Build privileged vector image

---
+++ vector/src/privileged.dockerfile
@@ -0,0 +1,23 @@
+﻿# This is free and unencumbered software released into the public domain.
+
+FROM docker.io/timberio/vector:0.49.0-debian AS vector
+
+FROM docker.io/library/golang:1.25.0-alpine AS healthcheck
+
+WORKDIR /go
+
+COPY vector/src/healthcheck.go healthcheck.go
+
+RUN go build -ldflags="-w -s" healthcheck.go
+RUN chmod ugo=rx /go/healthcheck
+
+FROM scratch
+
+COPY --from=vector /usr/local/bin/vector /usr/bin/vector
+COPY --from=healthcheck /go/healthcheck /usr/bin/healthcheck
+
+ENTRYPOINT ["/usr/bin/vector"]
+
+HEALTHCHECK CMD ["/usr/bin/healthcheck"]
+
+CMD ["--config", "/etc/vector/*.toml"]
---
+++ vector/test/privileged.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+# This is free and unencumbered software released into the public domain.
+
+import os, pytest, random, requests, subprocess
+
+name, port = random.sample(range(1025, 65536), 2)
+
+session = requests.Session()
+session.mount('http://', requests.adapters.HTTPAdapter(max_retries=10))
+
+@pytest.fixture(autouse=True, scope='session')
+def fixture():
+	dir = os.path.dirname(os.path.realpath(__file__))
+	tag = os.getenv('TAG') or 'latest'
+	subprocess.run([
+		'podman', 'run', '--detach', '--name', f'{name}', '--pull', 'never',
+		'--read-only', f'ghcr.io/ngarside/vector-privileged:{tag}',
+	])
+	yield
+	subprocess.run(['podman', 'rm', '--force', f'{name}'])
+
+def test_healthcheck():
+	status = subprocess.run(['podman', 'healthcheck', 'run', f'{name}'])
+	assert status.returncode == 0
