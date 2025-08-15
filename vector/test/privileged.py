d266259a264ec554342681d31bccf52d37c33e69
Nathan Garside <git@nathangarside.com>
2025-08-15 09:11:55 +0100

    Skip tests if image not found

--- vector/test/privileged.py
+++ vector/test/privileged.py
@@ -6,18 +6,22 @@ import os, pytest, random, requests, subprocess
 
 name, port = random.sample(range(1025, 65536), 2)
 
+dir = os.path.dirname(os.path.realpath(__file__))
+tag = os.getenv('TAG') or 'latest'
+image = f'ghcr.io/ngarside/vector-privileged:{tag}'
+
 session = requests.Session()
 session.mount('http://', requests.adapters.HTTPAdapter(max_retries=10))
 
+has_image = subprocess.run(['podman', 'image', 'inspect', image]).returncode
+pytestmark = pytest.mark.skipif(has_image != 0, reason=f'Image not found')
+
 @pytest.fixture(autouse=True, scope='session')
 def fixture():
-	dir = os.path.dirname(os.path.realpath(__file__))
-	tag = os.getenv('TAG') or 'latest'
 	subprocess.run([
 		'podman', 'run', '--detach', '--name', f'{name}', '--pull', 'never',
 		'--read-only', '--volume',
-		f'{dir}/vector.toml:/etc/vector/vector.toml:ro',
-		f'ghcr.io/ngarside/vector-privileged:{tag}', '--config',
+		f'{dir}/vector.toml:/etc/vector/vector.toml:ro', image, '--config',
 		'/etc/vector/vector.toml',
 	])
 	yield
