de9cdc8f622d3ccf9d28052ccd6152f0258f4b3f
Nathan Garside <git@nathangarside.com>
2025-08-19 14:00:27 +0100

    Prevent adding ISOs to git

--- ostree/setup.sh
+++ ostree/setup.sh
@@ -5,4 +5,4 @@ set -euo pipefail
 
 podman build --file ostree/src/ostree.dockerfile --format docker --tag ghcr.io/ngarside/server:feature_bootc .
 
-podman run --volume ./output:/output --privileged --volume /var/lib/containers/storage:/var/lib/containers/storage -it quay.io/centos-bootc/bootc-image-builder:latest --rootfs btrfs --type iso ghcr.io/ngarside/server:feature_bootc
+podman run --volume ./bin:/output --privileged --volume /var/lib/containers/storage:/var/lib/containers/storage -it quay.io/centos-bootc/bootc-image-builder:latest --rootfs btrfs --type iso ghcr.io/ngarside/server:feature_bootc
