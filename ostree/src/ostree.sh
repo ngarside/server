d6d6168152b54f507b66f3ca0e13da3494c4d2d9
Nathan Garside <git@nathangarside.com>
2025-09-22 12:58:47 +0100

    Enable passwordless sudo (#391)

--- ostree/src/ostree.sh
+++ ostree/src/ostree.sh
@@ -10,3 +10,7 @@ cp /tmp/git/ostree/ops/vconsole.conf /etc/vconsole.conf
 cp /tmp/git/ostree/ops/sysctl.conf /etc/sysctl.d/0-server.conf
 
 cp /tmp/git/ostree/ops/subuid.txt /etc/subuid
+
+mkdir --parents /etc/sudoers.d
+cp /tmp/git/ostree/ops/sudoers.conf /etc/sudoers.d/server
+chmod 0440 /etc/sudoers.d/server
