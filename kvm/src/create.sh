7c7b6986d52256c8733e206249aa821611a5f315
Nathan Garside <git@nathangarside.com>
2025-09-23 15:21:13 +0100

    Add create script

---
+++ kvm/src/create.sh
@@ -0,0 +1,12 @@
+# This is free and unencumbered software released into the public domain.
+
+if [[ "$USER" != "root" ]]; then
+	echo "Script must be run as superuser; exiting"
+	exit 1
+fi
+
+qemu-img create -f qcow2 /var/lib/libvirt/images/server-root.qcow2 64G
+qemu-img create -f qcow2 /var/lib/libvirt/images/server-data.qcow2 512G
+
+XML="$(dirname $BASH_SOURCE[0])/config.xml"
+virsh create "$XML"
