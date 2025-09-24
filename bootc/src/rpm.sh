e80a73215107d822610ba658d38c49f78ffc3f61
Nathan Garside <git@nathangarside.com>
2025-09-24 11:32:12 +0100

    Remove qemu (#442)

--- bootc/src/rpm.sh
+++ bootc/src/rpm.sh
@@ -15,6 +15,7 @@ dnf --assumeyes remove \
 	console-login-helper-messages-profile \
 	console-login-helper-messages-issuegen \
 	NetworkManager-cloud-setup \
+	qemu-user-static* \
 	python-pip-wheel \
 	python3 \
 	python3-libs \
