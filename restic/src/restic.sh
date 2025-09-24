37f8502c4b2bc3c55243da0afd450e695cb41d7c
Nathan Garside <git@nathangarside.com>
2025-09-24 15:08:55 +0100

    Split exporting

--- restic/src/restic.sh
+++ restic/src/restic.sh
@@ -4,11 +4,15 @@
 set -euo pipefail
 
 # Load environment variables
-export AWS_ACCESS_KEY_ID=$(podman secret inspect restic_repository_key --showsecret | jq --raw-output ".[].SecretData")
-export RESTIC_PASSWORD=$(podman secret inspect restic_repository_password --showsecret | jq --raw-output ".[].SecretData")
-export AWS_SECRET_ACCESS_KEY=$(podman secret inspect restic_repository_secret --showsecret | jq --raw-output ".[].SecretData")
-export RESTIC_REPOSITORY=$(podman secret inspect restic_repository_uri --showsecret | jq --raw-output ".[].SecretData")
-export RESTIC_REPOSITORY="s3:$RESTIC_REPOSITORY"
+AWS_ACCESS_KEY_ID=$(podman secret inspect restic_repository_key --showsecret | jq --raw-output ".[].SecretData")
+RESTIC_PASSWORD=$(podman secret inspect restic_repository_password --showsecret | jq --raw-output ".[].SecretData")
+AWS_SECRET_ACCESS_KEY=$(podman secret inspect restic_repository_secret --showsecret | jq --raw-output ".[].SecretData")
+RESTIC_REPOSITORY=$(podman secret inspect restic_repository_uri --showsecret | jq --raw-output ".[].SecretData")
+RESTIC_REPOSITORY="s3:$RESTIC_REPOSITORY"
+export AWS_ACCESS_KEY_ID
+export RESTIC_PASSWORD
+export AWS_SECRET_ACCESS_KEY
+export RESTIC_REPOSITORY
 
 # Delete dangling snapshot if it wasn't cleaned up correctly
 btrfs subvolume delete /var/data/@backup || true
