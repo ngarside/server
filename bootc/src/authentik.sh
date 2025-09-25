aaf745afa265ffcc759773d33f8e233e8bdc0d81
Nathan Garside <git@nathangarside.com>
2025-09-25 11:45:06 +0100

    Remove bootc prefixes

--- bootc/src/authentik.sh
+++ bootc/src/authentik.sh
@@ -7,15 +7,15 @@ mkdir --parents /etc/containers/systemd/users/1001
 mkdir --parents /etc/authentik/blueprints
 mkdir --parents /etc/authentik/media
 
-cp /git/authentik/ops/authentik_external.network /etc/containers/systemd/users/1001/authentik_external.network
-cp /git/authentik/ops/authentik_internal.network /etc/containers/systemd/users/1001/authentik_internal.network
-cp /git/authentik/ops/authentik_postgres.container /etc/containers/systemd/users/1001/authentik_postgres.container
-cp /git/authentik/ops/authentik_server.container /etc/containers/systemd/users/1001/authentik_server.container
-cp /git/authentik/ops/authentik_valkey.container /etc/containers/systemd/users/1001/authentik_valkey.container
-cp /git/authentik/ops/authentik_worker.container /etc/containers/systemd/users/1001/authentik_worker.container
-cp /git/authentik/ops/blueprints_memos.yaml /etc/authentik/blueprints/memos.yaml
-cp /git/authentik/ops/blueprints_youtrack.yaml /etc/authentik/blueprints/youtrack.yaml
-cp /git/authentik/ops/blueprints_outline.yaml /etc/authentik/blueprints/outline.yaml
-cp /git/authentik/ops/logo_memos.png /etc/authentik/media/memos.png
-cp /git/authentik/ops/logo_youtrack.png /etc/authentik/media/youtrack.png
-cp /git/authentik/ops/logo_outline.png /etc/authentik/media/outline.png
+cp /git/authentik/ops/external.network /etc/containers/systemd/users/1001/external.network
+cp /git/authentik/ops/internal.network /etc/containers/systemd/users/1001/internal.network
+cp /git/authentik/ops/postgres.container /etc/containers/systemd/users/1001/postgres.container
+cp /git/authentik/ops/server.container /etc/containers/systemd/users/1001/server.container
+cp /git/authentik/ops/valkey.container /etc/containers/systemd/users/1001/valkey.container
+cp /git/authentik/ops/worker.container /etc/containers/systemd/users/1001/worker.container
+cp /git/authentik/ops/memos.yaml /etc/authentik/blueprints/memos.yaml
+cp /git/authentik/ops/youtrack.yaml /etc/authentik/blueprints/youtrack.yaml
+cp /git/authentik/ops/outline.yaml /etc/authentik/blueprints/outline.yaml
+cp /git/authentik/ops/memos.png /etc/authentik/media/memos.png
+cp /git/authentik/ops/youtrack.png /etc/authentik/media/youtrack.png
+cp /git/authentik/ops/outline.png /etc/authentik/media/outline.png
