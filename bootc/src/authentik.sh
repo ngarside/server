62c02b4c2bd2cf1bcbcd675eb1b48909e36b315e
Nathan Garside <git@nathangarside.com>
2025-09-28 15:04:21 +0100

    Add provider for Gitea (#463)

--- bootc/src/authentik.sh
+++ bootc/src/authentik.sh
@@ -9,6 +9,7 @@ mkdir --parents /etc/authentik/media
 
 cp /git/authentik/ops/blueprints/adguardhome.yaml /etc/authentik/blueprints/adguardhome.yaml
 cp /git/authentik/ops/blueprints/fossflow.yaml /etc/authentik/blueprints/fossflow.yaml
+cp /git/authentik/ops/blueprints/gitea.yaml /etc/authentik/blueprints/gitea.yaml
 cp /git/authentik/ops/blueprints/memos.yaml /etc/authentik/blueprints/memos.yaml
 cp /git/authentik/ops/blueprints/outline.yaml /etc/authentik/blueprints/outline.yaml
 cp /git/authentik/ops/blueprints/penpot.yaml /etc/authentik/blueprints/penpot.yaml
