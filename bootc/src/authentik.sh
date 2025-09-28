ef57c208afb1f874dbb942e1e47b80135dc2a994
Nathan Garside <git@nathangarside.com>
2025-09-28 15:56:56 +0100

    Fix authentik outpost providers

--- bootc/src/authentik.sh
+++ bootc/src/authentik.sh
@@ -12,6 +12,7 @@ cp /git/authentik/ops/blueprints/fossflow.yaml /etc/authentik/blueprints/fossflo
 cp /git/authentik/ops/blueprints/gitea.yaml /etc/authentik/blueprints/gitea.yaml
 cp /git/authentik/ops/blueprints/memos.yaml /etc/authentik/blueprints/memos.yaml
 cp /git/authentik/ops/blueprints/outline.yaml /etc/authentik/blueprints/outline.yaml
+cp /git/authentik/ops/blueprints/outpost.yaml /etc/authentik/blueprints/outpost.yaml
 cp /git/authentik/ops/blueprints/penpot.yaml /etc/authentik/blueprints/penpot.yaml
 cp /git/authentik/ops/blueprints/vaultwarden.yaml /etc/authentik/blueprints/vaultwarden.yaml
 cp /git/authentik/ops/blueprints/youtrack.yaml /etc/authentik/blueprints/youtrack.yaml
