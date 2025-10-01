e2dab923d810f59b920de80241f6fdaf0736bf51
Nathan Garside <git@nathangarside.com>
2025-10-01 08:40:33 +0100

    Harden vaultwarden image (#508)

--- vaultwarden/test/vaultwarden.py
+++ vaultwarden/test/vaultwarden.py
@@ -2,5 +2,28 @@
 
 # This is free and unencumbered software released into the public domain.
 
-def test_stub():
-	assert True
+import os, pytest, random, requests, subprocess, time
+
+name, port = random.sample(range(1025, 65536), 2)
+
+session = requests.Session()
+session.mount('http://', requests.adapters.HTTPAdapter(max_retries=10))
+
+@pytest.fixture(autouse=True, scope='session')
+def fixture():
+	tag = os.getenv('TAG') or 'latest'
+	subprocess.run([
+		'podman', 'run', '--detach', '--name', f'{name}', '--publish',
+		f'{port}:80', '--pull', 'never', f'ghcr.io/ngarside/vaultwarden:{tag}',
+	])
+	time.sleep(1)
+	yield
+	subprocess.run(['podman', 'rm', '--force', f'{name}'])
+
+def test_healthcheck():
+	status = subprocess.run(['podman', 'healthcheck', 'run', f'{name}'])
+	assert status.returncode == 0
+
+def test_root():
+	res = session.get(f'http://localhost:{port}')
+	assert res.status_code == 200
