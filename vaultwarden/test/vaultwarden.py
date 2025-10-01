69f6a71786f6d40a754cb267fded21eeeaf0aabe
Nathan Garside <git@nathangarside.com>
2025-10-01 08:35:26 +0100

    Fix python lint

--- vaultwarden/test/vaultwarden.py
+++ vaultwarden/test/vaultwarden.py
@@ -2,7 +2,7 @@
 
 # This is free and unencumbered software released into the public domain.
 
-import os, pytest, random, requests, subprocess, tempfile, time
+import os, pytest, random, requests, subprocess, time
 
 name, port = random.sample(range(1025, 65536), 2)
 
@@ -11,7 +11,6 @@ session.mount('http://', requests.adapters.HTTPAdapter(max_retries=10))
 
 @pytest.fixture(autouse=True, scope='session')
 def fixture():
-	dir = tempfile.mkdtemp()
 	tag = os.getenv('TAG') or 'latest'
 	subprocess.run([
 		'podman', 'run', '--detach', '--name', f'{name}', '--publish',
