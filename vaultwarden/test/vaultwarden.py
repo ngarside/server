3c1d53dd3aac71b15d6550aae026234e320ef90d
Nathan Garside <git@nathangarside.com>
2025-10-01 08:19:29 +0100

    Add healthcheck test

--- vaultwarden/test/vaultwarden.py
+++ vaultwarden/test/vaultwarden.py
@@ -2,5 +2,25 @@
 
 # This is free and unencumbered software released into the public domain.
 
-def test_stub():
-	assert True
+import os, pytest, random, requests, subprocess, tempfile, time
+
+name, port = random.sample(range(1025, 65536), 2)
+
+session = requests.Session()
+session.mount('http://', requests.adapters.HTTPAdapter(max_retries=10))
+
+@pytest.fixture(autouse=True, scope='session')
+def fixture():
+	dir = tempfile.mkdtemp()
+	tag = os.getenv('TAG') or 'latest'
+	subprocess.run([
+		'podman', 'run', '--detach', '--name', f'{name}', '--publish',
+		f'{port}:80', '--pull', 'never', f'ghcr.io/ngarside/vaultwarden:{tag}',
+	])
+	time.sleep(0.1)
+	yield
+	subprocess.run(['podman', 'rm', '--force', f'{name}'])
+
+def test_healthcheck():
+	status = subprocess.run(['podman', 'healthcheck', 'run', f'{name}'])
+	assert status.returncode == 0
