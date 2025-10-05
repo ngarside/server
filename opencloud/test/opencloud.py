2033cb13236019339e9f0ee973c27d870a503227
Nathan Garside <git@nathangarside.com>
2025-10-05 15:45:27 +0100

    Add real test case

--- opencloud/test/opencloud.py
+++ opencloud/test/opencloud.py
@@ -2,5 +2,34 @@
 
 # This is free and unencumbered software released into the public domain.
 
-def test_stub():
-	assert True
+import os, pytest, random, requests, subprocess, tempfile, time
+
+etc = tempfile.TemporaryDirectory()
+name, port = random.sample(range(1025, 65536), 2)
+
+port = 9100
+
+session = requests.Session()
+session.mount('http://', requests.adapters.HTTPAdapter(max_retries=10))
+
+@pytest.fixture(autouse=True, scope='session')
+def fixture():
+	tag = os.getenv('TAG') or 'latest'
+	subprocess.run([
+		'podman', 'run', '--name', f'{name}', '--publish', f'{port}:80',
+		'--pull', 'never', '--rm', '--volume', f'/workspaces/Server/etc:/etc/opencloud',
+		f'ghcr.io/ngarside/opencloud:{tag}', 'init', '--admin-password', 'admin',
+		'--insecure', 'yes',
+	])
+	subprocess.run([
+		'podman', 'run', '--detach', '--name', f'{name}', '--publish', f'{port}:9200',
+		'--pull', 'never', '--volume', f'/workspaces/Server/etc:/etc/opencloud',
+		f'ghcr.io/ngarside/opencloud:{tag}',
+	])
+	time.sleep(5)
+	yield
+	subprocess.run(['podman', 'rm', '--force', f'{name}'])
+
+def test_home():
+	response = session.get(f'http://localhost:{port}', timeout=10)
+	assert response.status_code == 200
