d0038f7034e0083eed3240b5553bee995d8d0afc
Nathan Garside <git@nathangarside.com>
2025-10-07 20:42:06 +0100

    Hardcode gitea config (#533)

---
+++ gitea/src/entry.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is free and unencumbered software released into the public domain.
+
+gitea admin auth add-oauth \
+	--auto-discover-url "https://$MACHINE_DOMAIN_ROOT/application/o/gitea/.well-known/openid-configuration" \
+	--key gitea \
+	--name Authentik \
+	--provider openidConnect\
+	--secret "$GITEA_CLIENT_SECRET"
+
+/usr/local/bin/docker-entrypoint.sh "$@"
