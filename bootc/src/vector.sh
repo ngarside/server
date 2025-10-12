c128017139bad5eda30abd901598ce7d1df5f572
Nathan Garside <git@nathangarside.com>
2025-10-12 11:10:17 +0100

    Revert "Remove network"
    This reverts commit cb38c95ef4e94a78db365444511fad2f15ba059b.

--- bootc/src/vector.sh
+++ bootc/src/vector.sh
@@ -6,6 +6,7 @@ set -euo pipefail
 mkdir --parents /etc/containers/systemd/users/1001
 cp /git/vector/ops/containers/privileged.container /etc/containers/systemd/users/1001/vector_privileged.container
 cp /git/vector/ops/containers/unprivileged.container /etc/containers/systemd/users/1001/vector_unprivileged.container
+cp /git/vector/ops/networks/vector.network /etc/containers/systemd/users/1001/vector.network
 
 mkdir --parents /etc/vector
 cp /git/vector/ops/privileged /etc/vector/privileged
