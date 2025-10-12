21bc8d6b8912800e322587c7f615fe229e30f7d0
Nathan Garside <git@nathangarside.com>
2025-10-12 11:09:27 +0100

    Tidy penpot ops folder (#569)

--- bootc/src/penpot.sh
+++ bootc/src/penpot.sh
@@ -4,10 +4,12 @@
 set -euo pipefail
 
 mkdir --parents /etc/containers/systemd/users/1001
-cp /git/penpot/ops/backend.container /etc/containers/systemd/users/1001/penpot_backend.container
-cp /git/penpot/ops/exporter.container /etc/containers/systemd/users/1001/penpot_exporter.container
-cp /git/penpot/ops/frontend.container /etc/containers/systemd/users/1001/penpot_frontend.container
-cp /git/penpot/ops/postgres.container /etc/containers/systemd/users/1001/penpot_postgres.container
-cp /git/penpot/ops/valkey.container /etc/containers/systemd/users/1001/penpot_valkey.container
-cp /git/penpot/ops/penpot_backend.network /etc/containers/systemd/users/1001/penpot_backend.network
-cp /git/penpot/ops/penpot_frontend.network /etc/containers/systemd/users/1001/penpot_frontend.network
+
+cp /git/penpot/ops/containers/backend.container /etc/containers/systemd/users/1001/penpot_backend.container
+cp /git/penpot/ops/containers/exporter.container /etc/containers/systemd/users/1001/penpot_exporter.container
+cp /git/penpot/ops/containers/frontend.container /etc/containers/systemd/users/1001/penpot_frontend.container
+cp /git/penpot/ops/containers/postgres.container /etc/containers/systemd/users/1001/penpot_postgres.container
+cp /git/penpot/ops/containers/valkey.container /etc/containers/systemd/users/1001/penpot_valkey.container
+
+cp /git/penpot/ops/networks/internal.network /etc/containers/systemd/users/1001/penpot_internal.network
+cp /git/penpot/ops/networks/external.network /etc/containers/systemd/users/1001/penpot_external.network
