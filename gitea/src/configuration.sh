2e95d56c6b78b5427b7fa433915934ab5183bd4a
Nathan Garside <git@nathangarside.com>
2025-10-13 12:35:32 +0100

    Reduce line length

--- gitea/src/configuration.sh
+++ gitea/src/configuration.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # This is free and unencumbered software released into the public domain.
 
-# Wait for Gitea server to initialise --------------------------------------------------------------
+# Wait for Gitea server to initialise ----------------------------------------------------
 echo "[CONFIG] Waiting for Gitea server to initialise"
 RETRIES_MAX=20
 RETRIES_CURRENT=1
@@ -24,7 +24,7 @@ if (( RETRIES_CURRENT > RETRIES_MAX )); then
 fi
 echo "[CONFIG] Finished waiting for initialisation"
 
-# Read OIDC secret ---------------------------------------------------------------------------------
+# Read OIDC secret -----------------------------------------------------------------------
 echo "[CONFIG] Reading 'gitea_oidc_secret' secret"
 GITEA_OIDC_SECRET=$(cat /run/secrets/gitea_oidc_secret)
 if [ -z "$GITEA_OIDC_SECRET" ]; then
@@ -33,7 +33,7 @@ if [ -z "$GITEA_OIDC_SECRET" ]; then
 fi
 echo "[CONFIG] Secret 'gitea_oidc_secret' read successfully"
 
-# Read domain secret -------------------------------------------------------------------------------
+# Read domain secret ---------------------------------------------------------------------
 echo "[CONFIG] Reading 'machine_domain_root' secret"
 MACHINE_DOMAIN_ROOT=$(cat /run/secrets/machine_domain_root)
 if [ -z "$MACHINE_DOMAIN_ROOT" ]; then
@@ -42,10 +42,11 @@ if [ -z "$MACHINE_DOMAIN_ROOT" ]; then
 fi
 echo "[CONFIG] Secret 'machine_domain_root' read successfully"
 
-# Configure Authentik OIDC -------------------------------------------------------------------------
+# Configure Authentik OIDC ---------------------------------------------------------------
 echo "[CONFIG] Adding OIDC configuration for Gitea/Authentik"
 gitea admin auth add-oauth \
-	--auto-discover-url "https://$MACHINE_DOMAIN_ROOT/application/o/gitea/.well-known/openid-configuration" \
+	--auto-discover-url \
+	"https://$MACHINE_DOMAIN_ROOT/application/o/gitea/.well-known/openid-configuration" \
 	--key gitea \
 	--name Authentik \
 	--provider openidConnect \
