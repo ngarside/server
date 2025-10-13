68859f5cf064a11064272ca5cf7f2c7bb2e91d51
Nathan Garside <git@nathangarside.com>
2025-10-13 14:45:54 +0100

    Add tests for alpine (#593)

--- alpine/test/alpine.py
+++ alpine/test/alpine.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
 # This is free and unencumbered software released into the public domain.
 
-def test_stub():
-	assert True
+import os, subprocess
+
+image = f'ghcr.io/ngarside/alpine:{os.getenv('TAG') or 'latest'}'
+
+def test_whoami():
+	status = subprocess.run(['podman', 'run', image, 'whoami'], capture_output=True)
+	assert status.stdout == b'root\n'
