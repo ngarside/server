37b81cc183a54b9103cca4770f568ee4a0805fd9
Nathan Garside <git@nathangarside.com>
2025-10-17 13:52:20 +0100

    Sort ops common files

--- bootc/src/bootc.sh
+++ bootc/src/bootc.sh
@@ -5,22 +5,22 @@ set -euo pipefail
 
 mkdir --parents /etc/sysctl.d
 
-cp /git/bootc/ops/vconsole.conf /etc/vconsole.conf
+cp /git/bootc/ops/common/vconsole.conf /etc/vconsole.conf
 
-cp /git/bootc/ops/sysctl.conf /etc/sysctl.d/0-server.conf
+cp /git/bootc/ops/common/sysctl.conf /etc/sysctl.d/0-server.conf
 
-cp /git/bootc/ops/subuid.txt /etc/subuid
+cp /git/bootc/ops/common/subuid.txt /etc/subuid
 
-cp /git/bootc/ops/fstab.conf /etc/fstab
+cp /git/bootc/ops/common/fstab.conf /etc/fstab
 
 mkdir --parents /etc/sudoers.d
-cp /git/bootc/ops/sudoers.conf /etc/sudoers.d/server
+cp /git/bootc/ops/common/sudoers.conf /etc/sudoers.d/server
 chmod 0440 /etc/sudoers.d/server
 
-cp /git/bootc/ops/root.conf /usr/lib/ostree/prepare-root.conf
+cp /git/bootc/ops/common/root.conf /usr/lib/ostree/prepare-root.conf
 KERNEL=$(cd /usr/lib/modules && echo *)
 dracut --force --verbose "/usr/lib/modules/$KERNEL/initramfs.img" "$KERNEL"
 
-cp /git/bootc/ops/data.service /usr/lib/systemd/system/data-chown.service
+cp /git/bootc/ops/common/data.service /usr/lib/systemd/system/data-chown.service
 
 systemctl enable data-chown.service
