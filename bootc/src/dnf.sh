5ee39d828fb60e2bb361187a375e263ae5db979b
Nathan Garside <git@nathangarside.com>
2025-10-18 15:09:34 +0100

    Remove cracklib packages

--- bootc/src/dnf.sh
+++ bootc/src/dnf.sh
@@ -16,6 +16,7 @@ dnf --assumeyes remove \
 	console-login-helper-messages \
 	console-login-helper-messages-profile \
 	console-login-helper-messages-issuegen \
+	cracklib* \
 	nano \
 	nano-default-editor \
 	flatpak-session-helper \
