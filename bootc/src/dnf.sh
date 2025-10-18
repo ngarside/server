49552952d3d982ba6c5ac9807e368b2958208c36
Nathan Garside <git@nathangarside.com>
2025-10-18 15:26:50 +0100

    Remove irrelevant firmware (#663)

--- bootc/src/dnf.sh
+++ bootc/src/dnf.sh
@@ -8,21 +8,27 @@ dnf --assumeyes install restic
 dnf --assumeyes remove \
 	adwaita-mono-fonts \
 	adwaita-sans-fonts \
+	atheros-firmware \
 	amd-gpu-firmware \
 	amd-ucode-firmware \
 	bash-completion \
 	bluez \
+	brcmfmac-firmware \
+	cirrus-audio-firmware \
 	cloud-utils-growpart \
 	console-login-helper-messages \
 	console-login-helper-messages-profile \
 	console-login-helper-messages-issuegen \
+	mt7xxx-firmware \
 	nano \
 	nano-default-editor \
+	nxpwireless-firmware \
 	flatpak-session-helper \
 	fonts-filesystem \
 	NetworkManager-cloud-setup \
 	NetworkManager-tui \
 	ntfs* \
+	qcom-wwan-firmware \
 	qemu-user-static* \
 	python-pip-wheel \
 	python3 \
@@ -30,6 +36,7 @@ dnf --assumeyes remove \
 	rpm-ostree* \
 	samba* \
 	sudo-python-plugin \
+	tiwilink-firmware \
 	toolbox \
 	nvidia-gpu-firmware
 
