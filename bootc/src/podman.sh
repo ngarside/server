e5c14ff13593b8385bb4df628e94aece440dff71
Nathan Garside <git@nathangarside.com>
2025-10-19 12:03:47 +0100

    Use transient container storage (#680)

--- bootc/src/podman.sh
+++ bootc/src/podman.sh
@@ -4,3 +4,7 @@
 set -euo pipefail
 
 cp /git/bootc/ops/podman/policy.json /etc/containers/policy.json
+cp /git/bootc/ops/podman/prune.service /usr/lib/systemd/system/podman_prune.service
+cp /git/bootc/ops/podman/storage.conf /etc/containers/storage.conf
+
+systemctl enable podman_prune
