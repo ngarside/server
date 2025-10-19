ea11e2fc8045ee7e23893dee53d2532fadbb3431
Nathan Garside <git@nathangarside.com>
2025-10-19 10:52:36 +0100

    Copy to var home folder

--- bootc/src/podman.sh
+++ bootc/src/podman.sh
@@ -4,7 +4,7 @@
 set -euo pipefail
 
 mkdir --parents /home/containers/.config/systemd/user
-cp /git/bootc/ops/podman/prune.service /home/containers/.config/systemd/user/podman_prune.service
+cp /git/bootc/ops/podman/prune.service /var/home/containers/.config/systemd/user/podman_prune.service
 
 cp /git/bootc/ops/podman/policy.json /etc/containers/policy.json
 cp /git/bootc/ops/podman/storage.conf /etc/containers/storage.conf
