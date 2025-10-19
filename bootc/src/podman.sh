8221397634f02578378a0f37edefcaf2f3a4cc8f
Nathan Garside <git@nathangarside.com>
2025-10-19 11:36:49 +0100

    Enable prune service

--- bootc/src/podman.sh
+++ bootc/src/podman.sh
@@ -6,3 +6,5 @@ set -euo pipefail
 cp /git/bootc/ops/podman/policy.json /etc/containers/policy.json
 cp /git/bootc/ops/podman/prune.service /usr/lib/systemd/system/podman_prune.service
 cp /git/bootc/ops/podman/storage.conf /etc/containers/storage.conf
+
+systemctl enable podman_prune
