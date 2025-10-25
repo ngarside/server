d9eb18c2c15f42b7e2375d635ee0ad29eb4c7f8a
Nathan Garside <git@nathangarside.com>
2025-10-25 18:44:29 +0100

    Disable firewire

--- bootc/src/modprobe.sh
+++ bootc/src/modprobe.sh
@@ -3,5 +3,7 @@
 
 set -euo pipefail
 
+echo 'install firewire-ohci /bin/true' > /etc/modprobe.d/firewire-ohci.conf
+echo 'install firewire-sbp2 /bin/true' > /etc/modprobe.d/firewire-sbp2.conf
 echo 'install sctp /bin/true' > /etc/modprobe.d/sctp.conf
 echo 'install tipc /bin/true' > /etc/modprobe.d/tipc.conf
