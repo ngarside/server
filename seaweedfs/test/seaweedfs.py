49135ef1ee9c9e142ef1682b2ecec5c90f7443ac
Nathan Garside <git@nathangarside.com>
2025-10-27 13:02:17 +0000

    Simplify creating client

--- seaweedfs/test/seaweedfs.py
+++ seaweedfs/test/seaweedfs.py
@@ -8,9 +8,15 @@ BUCKET_NAME = 'my-bucket'
 name, port = random.sample(range(1025, 65536), 2)
 port = 8333
 
+client = boto3.client(
+	's3',
+	aws_access_key_id='',
+	aws_secret_access_key='',
+	endpoint_url=f'http://localhost:{port}',
+)
+
 @pytest.fixture(autouse=True, scope='session')
 def fixture():
-	global client
 	tag = os.getenv('TAG') or 'latest'
 	subprocess.run([
 		'podman', 'run', '--detach', '--name', f'{name}', '--publish',
@@ -19,12 +25,6 @@ def fixture():
 	])
 	for _ in range(10):
 		try:
-			client = boto3.client(
-				's3',
-				aws_access_key_id='',
-				aws_secret_access_key='',
-				endpoint_url=f'http://localhost:{port}',
-			)
 			client.create_bucket(Bucket=BUCKET_NAME)
 			break
 		except:
