6f2facfba5bcc4ac0fe99ae64ba395827a76f584
Nathan Garside <git@nathangarside.com>
2025-10-27 13:16:37 +0000

    Mirror seaweedfs image (#738)

---
+++ seaweedfs/readme.md
@@ -0,0 +1,14 @@
+<!-- This is free and unencumbered software released into the public domain -->
+
+# <p align=center>SeaweedFS
+
+A mirror of the official SeaweedFS container image.
+
+# <p align=center>References
+
+- [github.com/seaweedfs/seaweedfs](https://github.com/seaweedfs/seaweedfs)
+- [seaweedfs.com](https://seaweedfs.com)
+
+# <p align=center>License
+
+This is free and unencumbered software released into the public domain.
---
+++ seaweedfs/test/seaweedfs.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# This is free and unencumbered software released into the public domain.
+
+import boto3, os, pytest, random, subprocess, time
+
+name, port = random.sample(range(1025, 65536), 2)
+client = boto3.client(
+	's3', aws_access_key_id='', aws_secret_access_key='',
+	endpoint_url=f'http://localhost:{port}',
+)
+
+@pytest.fixture(autouse=True, scope='session')
+def fixture():
+	tag = os.getenv('TAG') or 'latest'
+	subprocess.run([
+		'podman', 'run', '--detach', '--name', f'{name}', '--publish',
+		f'{port}:8333', '--pull', 'never', f'ghcr.io/ngarside/seaweedfs:{tag}',
+		'server', '-s3',
+	])
+	for _ in range(10):
+		try:
+			client.create_bucket(Bucket='test')
+			break
+		except:
+			time.sleep(1)
+	yield
+	subprocess.run(['podman', 'rm', '--force', f'{name}'])
+
+def test_bucket_list():
+	bucket = client.list_buckets()['Buckets'][0]
+	assert bucket['Name'] == 'test'
