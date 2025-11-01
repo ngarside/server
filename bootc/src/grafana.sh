d75b67cf3a204dc02ab113ae48c04c56266f2ad8
Nathan Garside <git@nathangarside.com>
2025-11-01 17:13:13 +0000

    Run grafana instance

---
+++ bootc/src/grafana.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# This is free and unencumbered software released into the public domain.
+
+set -euo pipefail
+
+mkdir --parents /etc/containers/systemd/users/1001
+cp /git/grafana/ops/* /etc/containers/systemd/users/1001/
