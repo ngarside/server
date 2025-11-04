c12b8529bbfd0d1a977b19edd8900e08d4f3867c
Nathan Garside <git@nathangarside.com>
2025-11-04 19:02:04 +0000

    Enable dry run again

--- renovate/runner/config.js
+++ renovate/runner/config.js
@@ -1,6 +1,7 @@
 // This is free and unencumbered software released into the public domain.
 
 module.exports = {
+	dryRun: 'full',
 	gitAuthor: 'renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>',
 	repositories: ['ngarside/server'],
 	username: 'renovate[bot]',
