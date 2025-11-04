99c9922d20e0eb60a6bad2a50ee1375868803218
Nathan Garside <git@nathangarside.com>
2025-11-04 19:21:53 +0000

    Revert dry run

--- renovate/runner/config.js
+++ renovate/runner/config.js
@@ -1,7 +1,6 @@
 // This is free and unencumbered software released into the public domain.
 
 module.exports = {
-	dryRun: 'full',
 	gitAuthor: 'renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>',
 	repositories: ['ngarside/server'],
 };
