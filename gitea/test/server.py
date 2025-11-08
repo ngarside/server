f361628c7caa28bc823180a0b1e0a1fdee5d6605
Nathan Garside <git@nathangarside.com>
2025-11-08 20:54:59 +0000

    Use read-write etc volume

--- gitea/test/server.py
+++ gitea/test/server.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 # This is free and unencumbered software released into the public domain.
 
-import os, pytest, random, requests, subprocess, time
+import os, pytest, random, requests, subprocess, tempfile, time
 
+etc = tempfile.TemporaryDirectory()
 name, port = random.sample(range(1025, 65536), 2)
 
 session = requests.Session()
@@ -10,13 +11,12 @@ session.mount('http://', requests.adapters.HTTPAdapter(max_retries=10))
 
 @pytest.fixture(autouse=True, scope='session')
 def fixture():
-	dir = os.path.dirname(os.path.realpath(__file__))
+	open(os.path.join(etc.name, 'gitea.tmpl'), 'w').close()
 	tag = os.getenv('TAG') or 'latest'
 	subprocess.run([
 		'podman', 'run', '--detach', '--name', f'{name}', '--publish',
 		f'{port}:3000', '--pull', 'never', '--read-only', '--volume',
-		f'{dir}/server.tmpl:/etc/gitea/gitea.tmpl:ro',
-		f'ghcr.io/ngarside/gitea-server:{tag}',
+		f'{etc.name}:/etc/gitea', f'ghcr.io/ngarside/gitea-server:{tag}',
 	])
 	time.sleep(10)
 	yield
