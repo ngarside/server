1e9deef2331d7a796c2b699bebffce518940b384
Nathan Garside <git@nathangarside.com>
2025-11-09 23:33:40 +0000

    Run scripts using busybox shell (#919)

--- gitea/src/configuration.sh
+++ gitea/src/configuration.sh
@@ -1,52 +1,69 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 # This is free and unencumbered software released into the public domain.
 
-set -euo pipefail
+set -eu
 
 # Wait for Gitea server to initialise --------------------------------------------------------------
 echo "[CONFIG] Waiting for Gitea server to initialise"
 RETRIES_MAX=60
 RETRIES_CURRENT=1
-while (( RETRIES_CURRENT <= RETRIES_MAX )); do
+while [ "$RETRIES_CURRENT" -le "$RETRIES_MAX" ]; do
 	echo "[CONFIG] Sending HTTP request (attempt $RETRIES_CURRENT of $RETRIES_MAX)"
 	STATUS=$(wget -qSO /dev/null 0.0.0.0 2>&1 | awk '/^  HTTP/{print $2; exit}' | tr -d '\n')
 	echo "[CONFIG] Received $STATUS status code"
-	if (( STATUS >= 200 && STATUS < 400 )); then
-		echo "[CONFIG] Code is successful, exiting loop"
-		break
-	else
-		echo "[CONFIG] Code is unsuccessful, delaying before retry"
-		sleep 1
-	fi
-	(( RETRIES_CURRENT++ ))
+	case "$STATUS" in
+		2*|3*)
+			echo "[CONFIG] Code is successful, exiting loop"
+			break
+			;;
+		*)
+			echo "[CONFIG] Code is unsuccessful, delaying before retry"
+			sleep 1
+			;;
+	esac
+	RETRIES_CURRENT=$((RETRIES_CURRENT + 1))
 done
-if (( RETRIES_CURRENT > RETRIES_MAX )); then
+
+if [ "$RETRIES_CURRENT" -gt "$RETRIES_MAX" ]; then
 	echo "[CONFIG] Exceeded maximum retry count, aborting script"
 	exit 1
 fi
+
 echo "[CONFIG] Finished waiting for initialisation"
 
 # Read OIDC secret ---------------------------------------------------------------------------------
 echo "[CONFIG] Reading 'gitea_oidc_secret' secret"
-GITEA_OIDC_SECRET=$(cat /run/secrets/gitea_oidc_secret)
+if [ -f /run/secrets/gitea_oidc_secret ]; then
+	GITEA_OIDC_SECRET=$(cat /run/secrets/gitea_oidc_secret)
+else
+	GITEA_OIDC_SECRET=""
+fi
+
 if [ -z "$GITEA_OIDC_SECRET" ]; then
 	echo "[CONFIG] Secret 'gitea_oidc_secret' has not been set - exiting"
-	exit
+	exit 1
 fi
 echo "[CONFIG] Secret 'gitea_oidc_secret' read successfully"
 
 # Read domain secret -------------------------------------------------------------------------------
 echo "[CONFIG] Reading 'machine_domain_root' secret"
-MACHINE_DOMAIN_ROOT=$(cat /run/secrets/machine_domain_root)
+if [ -f /run/secrets/machine_domain_root ]; then
+	MACHINE_DOMAIN_ROOT=$(cat /run/secrets/machine_domain_root)
+else
+	MACHINE_DOMAIN_ROOT=""
+fi
+
 if [ -z "$MACHINE_DOMAIN_ROOT" ]; then
 	echo "[CONFIG] Secret 'machine_domain_root' has not been set - exiting"
-	exit
+	exit 1
 fi
 echo "[CONFIG] Secret 'machine_domain_root' read successfully"
 
 # Configure Authentik OIDC -------------------------------------------------------------------------
 echo "[CONFIG] Adding OIDC configuration for Gitea/Authentik"
-gitea admin auth add-oauth \
+gitea \
+	--config /tmp/gitea.ini \
+	admin auth add-oauth \
 	--auto-discover-url \
 	"https://$MACHINE_DOMAIN_ROOT/application/o/gitea/.well-known/openid-configuration" \
 	--key gitea \
--- gitea/src/entrypoint.sh
+++ gitea/src/entrypoint.sh
@@ -1,11 +1,11 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 # This is free and unencumbered software released into the public domain.
 
 # Custom entrypoint which starts a configuration script in the background
 # before calling the original entrypoint.
 
-set -euo pipefail
+set -eu
 
-/usr/bin/configuration &
 /usr/bin/telae /etc/gitea/gitea.tmpl /tmp/gitea.ini
+/usr/bin/configuration &
 /usr/bin/gitea --config /tmp/gitea.ini "$@"
