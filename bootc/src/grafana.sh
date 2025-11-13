9d0ea5ca176c381b8b0b7605ade58182c4e9a6b3
Nathan Garside <git@nathangarside.com>
2025-11-13 20:56:28 +0000

    Use OIDC signin with Grafana (#947)

--- bootc/src/grafana.sh
+++ bootc/src/grafana.sh
@@ -3,5 +3,9 @@
 
 set -euo pipefail
 
+mkdir --parents /etc/grafana
+cp /git/grafana/ops/grafana.ini /etc/grafana/grafana.ini
+
 mkdir --parents /etc/containers/systemd/users/1001
-cp /git/grafana/ops/* /etc/containers/systemd/users/1001/
+cp /git/grafana/ops/grafana.container /etc/containers/systemd/users/1001/grafana.container
+cp /git/grafana/ops/grafana.network /etc/containers/systemd/users/1001/grafana.network
