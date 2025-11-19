30a31bc0e5f26ffdbd57abd7a4b3acb683eb51fb
Nathan Garside <git@nathangarside.com>
2025-11-19 13:24:16 +0000

    Add testfile helper

---
+++ scripts/test/specifier_to_testfile_test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# This is free and unencumbered software released into the public domain.
+
+import sys
+from pathlib import Path
+
+sys.path.append(str(Path(__file__).parent.parent / 'src'))
+import specifier_to_testfile
+
+def test_with_image():
+	actual = specifier_to_testfile.specifier_to_testfile('service/image')
+	assert actual == 'service/test/image.py'
+
+def test_without_image():
+	actual = specifier_to_testfile.specifier_to_testfile('service')
+	assert actual == 'service/test/service.py'
