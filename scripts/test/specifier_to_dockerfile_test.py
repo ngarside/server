b5a25d6fb627ded8785f368316e9ea01fd0bfdb5
Nathan Garside <git@nathangarside.com>
2025-11-19 13:40:12 +0000

    Add extra docker tests

--- scripts/test/specifier_to_dockerfile_test.py
+++ scripts/test/specifier_to_dockerfile_test.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python
 # This is free and unencumbered software released into the public domain.
 
-import sys
+import pytest, sys
 from pathlib import Path
 
 sys.path.append(str(Path(__file__).parent.parent / 'src'))
 import specifier_to_dockerfile
+
+def test_empty():
+	with pytest.raises(ValueError):
+		specifier_to_dockerfile.specifier_to_dockerfile('')
+
+def test_extra_part():
+	with pytest.raises(ValueError):
+		specifier_to_dockerfile.specifier_to_dockerfile('service/image/component')
 
 def test_with_image():
 	actual = specifier_to_dockerfile.specifier_to_dockerfile('service/image')
