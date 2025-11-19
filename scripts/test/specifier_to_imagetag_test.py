1d1cf2687c579a5c1b16792f5668d48aafc43a71
Nathan Garside <git@nathangarside.com>
2025-11-19 13:40:49 +0000

    Add missing test cases

--- scripts/test/specifier_to_imagetag_test.py
+++ scripts/test/specifier_to_imagetag_test.py
@@ -7,6 +7,14 @@ from pathlib import Path
 sys.path.append(str(Path(__file__).parent.parent / 'src'))
 import specifier_to_imagetag
 
+def test_empty():
+	with pytest.raises(ValueError):
+		specifier_to_imagetag.specifier_to_imagetag('')
+
+def test_extra_part():
+	with pytest.raises(ValueError):
+		specifier_to_imagetag.specifier_to_imagetag('service/image/component')
+
 def test_with_image():
 	actual = specifier_to_imagetag.specifier_to_imagetag('service/image')
 	assert actual == 'ghcr.io/ngarside/service-image'
--- scripts/test/specifier_to_testfile_test.py
+++ scripts/test/specifier_to_testfile_test.py
@@ -7,6 +7,14 @@ from pathlib import Path
 sys.path.append(str(Path(__file__).parent.parent / 'src'))
 import specifier_to_testfile
 
+def test_empty():
+	with pytest.raises(ValueError):
+		specifier_to_testfile.specifier_to_testfile('')
+
+def test_extra_part():
+	with pytest.raises(ValueError):
+		specifier_to_testfile.specifier_to_testfile('service/image/component')
+
 def test_with_image():
 	actual = specifier_to_testfile.specifier_to_testfile('service/image')
 	assert actual == 'service/test/image.py'
