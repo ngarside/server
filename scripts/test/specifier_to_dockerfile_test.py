43048f6ba3acde6d7229ac98e32cc54ed06a3990
Nathan Garside <git@nathangarside.com>
2025-11-19 13:13:44 +0000

    Split test methods

--- scripts/test/specifier_to_dockerfile_test.py
+++ scripts/test/specifier_to_dockerfile_test.py
@@ -5,12 +5,12 @@ import sys
 from pathlib import Path
 
 sys.path.append(str(Path(__file__).parent.parent / 'src'))
-import pytest, specifier_to_dockerfile
+import specifier_to_dockerfile
 
-@pytest.mark.parametrize('input, expected', [
-	('service', 'service/src/service.dockerfile'), # Shared name.
-	('service/image', 'service/src/image.dockerfile'), # Different names.
-])
+def test_with_image():
+	actual = specifier_to_dockerfile.specifier_to_dockerfile('service/image')
+	assert actual == 'service/src/image.dockerfile'
 
-def test_sanitize(input, expected):
-	assert specifier_to_dockerfile.specifier_to_dockerfile(input) == expected
+def test_without_image():
+	actual = specifier_to_dockerfile.specifier_to_dockerfile('service')
+	assert actual == 'service/src/service.dockerfile'
