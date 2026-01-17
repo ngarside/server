b13ce88380cb887d7871770307a71dbbfa11eb7c
Nathan Garside <git@nathangarside.com>
2026-01-17 08:55:05 +0000

    Add container to system

--- bootc/src/gitea.sh
+++ bootc/src/gitea.sh
@@ -4,6 +4,7 @@
 set -euo pipefail
 
 mkdir --parents /etc/containers/systemd/users/1001
+cp /git/gitea/ops/runner.container /etc/containers/systemd/users/1001/gitea_runner.container
 cp /git/gitea/ops/server.container /etc/containers/systemd/users/1001/gitea_server.container
 cp /git/gitea/ops/server.network /etc/containers/systemd/users/1001/gitea_server.network
 
