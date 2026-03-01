206887daa00f2fdadb94ea13f9d5ae82ec409314
Nathan Garside <git@nathangarside.com>
2026-03-01 13:36:49 +0000

    Purge unused images (#1685)

--- bootc/src/podman.sh
+++ bootc/src/podman.sh
@@ -4,7 +4,10 @@
 set -euo pipefail
 
 cp /git/bootc/ops/podman/policy.json /etc/containers/policy.json
-cp /git/bootc/ops/podman/prune.service /usr/lib/systemd/system/podman_prune.service
+cp /git/bootc/ops/podman/prune_images.service /usr/lib/systemd/system/prune_images.service
+cp /git/bootc/ops/podman/prune_images.timer /usr/lib/systemd/system/prune_images.timer
+cp /git/bootc/ops/podman/prune_system.service /usr/lib/systemd/system/prune_system.service
 cp /git/bootc/ops/podman/storage.conf /etc/containers/storage.conf
 
-systemctl enable podman_prune
+systemctl enable prune_images.timer
+systemctl enable prune_system
