c03981547c231848bb0a0d596dbcfeec58b00412
Nathan Garside <git@nathangarside.com>
2026-04-12 11:55:13 +0100

    Create other data drives

--- kvm/src/create.sh
+++ kvm/src/create.sh
@@ -8,21 +8,31 @@ set -euo pipefail
 
 qemu-img create -f qcow2 /var/lib/libvirt/images/server-sda.qcow2 240G
 
-# Create data disk image -------------------------------------------------------
+# Create data disk images ------------------------------------------------------
 
 qemu-img create -f qcow2 /var/lib/libvirt/images/server-nvme0n1.qcow2 2T
+qemu-img create -f qcow2 /var/lib/libvirt/images/server-nvme1n1.qcow2 2T
+qemu-img create -f qcow2 /var/lib/libvirt/images/server-nvme2n1.qcow2 2T
 
 modprobe nbd max_part=8
 
 qemu-nbd --connect /dev/nbd0 /var/lib/libvirt/images/server-nvme0n1.qcow2
+qemu-nbd --connect /dev/nbd1 /var/lib/libvirt/images/server-nvme1n1.qcow2
+qemu-nbd --connect /dev/nbd2 /var/lib/libvirt/images/server-nvme2n1.qcow2
 
 parted /dev/nbd0 mklabel gpt
+parted /dev/nbd1 mklabel gpt
+parted /dev/nbd2 mklabel gpt
 
 parted --align optimal /dev/nbd0 mkpart primary 0% 100%
+parted --align optimal /dev/nbd1 mkpart primary 0% 100%
+parted --align optimal /dev/nbd2 mkpart primary 0% 100%
 
-mkfs.btrfs --force --label data /dev/nbd0p1
+mkfs.btrfs --force --label data /dev/nbd0p1 /dev/nbd1p1 /dev/nbd2p1
 
 qemu-nbd --disconnect /dev/nbd0
+qemu-nbd --disconnect /dev/nbd1
+qemu-nbd --disconnect /dev/nbd2
 
 # Create virtual machine -------------------------------------------------------
 
