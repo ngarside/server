b099dfbeb4576b633d20c5b60dcff6178b49c1a6
Nathan Garside <git@nathangarside.com>
2026-04-13 17:28:14 +0100

    Add config file to host

--- bootc/src/crowdsec.sh
+++ bootc/src/crowdsec.sh
@@ -3,5 +3,8 @@
 
 set -euo pipefail
 
+mkdir --parents /etc/crowdsec
 mkdir --parents /etc/containers/systemd/users/1001
+
+cp /git/crowdsec/ops/crowdsec.yaml /etc/crowdsec/acquis.yaml
 cp /git/crowdsec/ops/* /etc/containers/systemd/users/1001/
