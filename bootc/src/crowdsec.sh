38690b316f0134331d610c4d1670ea63766c9e50
Nathan Garside <git@nathangarside.com>
2026-04-14 03:41:56 +0100

    Fix copying files

--- bootc/src/crowdsec.sh
+++ bootc/src/crowdsec.sh
@@ -6,5 +6,6 @@ set -euo pipefail
 mkdir --parents /etc/crowdsec
 mkdir --parents /etc/containers/systemd/users/1001
 
-cp /git/crowdsec/ops/crowdsec.yaml /etc/crowdsec/acquis.yaml
-cp /git/crowdsec/ops/* /etc/containers/systemd/users/1001/
+cp /git/crowdsec/ops/crowdsec.yaml /etc/crowdsec/crowdsec.yaml
+cp /git/crowdsec/ops/crowdsec.container /etc/containers/systemd/users/1001/crowdsec.container
+cp /git/crowdsec/ops/crowdsec.network /etc/containers/systemd/users/1001/crowdsec.network
