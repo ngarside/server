34d5e70bad9ae8a263cedb9c571a14133d99d747
Nathan Garside <git@nathangarside.com>
2026-04-18 16:56:40 +0100

    Enable crowdsec (#1988)

--- bootc/src/crowdsec.sh
+++ bootc/src/crowdsec.sh
@@ -3,5 +3,9 @@
 
 set -euo pipefail
 
+mkdir --parents /etc/crowdsec
 mkdir --parents /etc/containers/systemd/users/1001
-cp /git/crowdsec/ops/* /etc/containers/systemd/users/1001/
+
+cp --recursive /git/crowdsec/ops/configuration/* /etc/crowdsec/
+cp --recursive /git/crowdsec/ops/containers/* /etc/containers/systemd/users/1001/
+cp --recursive /git/crowdsec/ops/networks/* /etc/containers/systemd/users/1001/
