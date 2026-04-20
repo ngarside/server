aa23dd558cd2df6979fb17554d43eeb719d50d56
Nathan Garside <git@nathangarside.com>
2026-04-20 17:52:33 +0100

    Add home directory

---
+++ bootc/src/tmpfiles.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# This is free and unencumbered software released into the public domain.
+
+set -euo pipefail
+
+mkdir --parents /etc/tmpfiles.d
+cp /git/bootc/ops/tmpfiles/demo.conf /etc/tmpfiles.d/demo.conf
