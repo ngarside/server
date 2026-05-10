1cd956f5c92521fac5859feeab06608fd6f9c770
Nathan Garside <git@nathangarside.com>
2026-05-10 11:55:26 +0100

    Use real links

--- bootc/src/sshd.sh
+++ bootc/src/sshd.sh
@@ -12,13 +12,12 @@ cp /git/bootc/ops/sshd/authorizedkeys.conf /etc/ssh/sshd_config.d/20-authorizedk
 cp /git/bootc/ops/sshd/hardening.conf /etc/ssh/sshd_config.d/10-hardening.conf
 cp /git/bootc/ops/sshd/authorizedkeys.pub /etc/ssh/authorized_keys/nathan
 
-ln --symbolic /var/ssh/test_key /etc/ssh/ssh_host_ecdsa_key
-# ln --symbolic /var/ssh/ssh_host_ecdsa_key /etc/ssh/ssh_host_ecdsa_key
-# ln --symbolic /var/ssh/ssh_host_ecdsa_key.pub /etc/ssh/ssh_host_ecdsa_key.pub
-# ln --symbolic /var/ssh/ssh_host_ed25519_key /etc/ssh/ssh_host_ed25519_key
-# ln --symbolic /var/ssh/ssh_host_ed25519_key.pub /etc/ssh/ssh_host_ed25519_key.pub
-# ln --symbolic /var/ssh/ssh_host_rsa_key /etc/ssh/ssh_host_rsa_key
-# ln --symbolic /var/ssh/ssh_host_rsa_key.pub /etc/ssh/ssh_host_rsa_key.pub
+ln --symbolic /var/ssh/ssh_host_ecdsa_key /etc/ssh/ssh_host_ecdsa_key
+ln --symbolic /var/ssh/ssh_host_ecdsa_key.pub /etc/ssh/ssh_host_ecdsa_key.pub
+ln --symbolic /var/ssh/ssh_host_ed25519_key /etc/ssh/ssh_host_ed25519_key
+ln --symbolic /var/ssh/ssh_host_ed25519_key.pub /etc/ssh/ssh_host_ed25519_key.pub
+ln --symbolic /var/ssh/ssh_host_rsa_key /etc/ssh/ssh_host_rsa_key
+ln --symbolic /var/ssh/ssh_host_rsa_key.pub /etc/ssh/ssh_host_rsa_key.pub
 
 chmod u=rwx,go=rx /usr/bin/keygen
 
