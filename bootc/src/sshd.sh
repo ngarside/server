201cabbf6eb48e39bf055d930a3b268996b2cc71
Nathan Garside <git@nathangarside.com>
2026-05-10 12:19:13 +0100

    Change hostkeys instead of setting links

--- bootc/src/sshd.sh
+++ bootc/src/sshd.sh
@@ -10,21 +10,8 @@ cp /git/bootc/ops/sshd/keygen.sh /usr/bin/keygen
 cp /git/bootc/ops/sshd/keygen.service /usr/lib/systemd/system/keygen.service
 cp /git/bootc/ops/sshd/authorizedkeys.conf /etc/ssh/sshd_config.d/20-authorizedkeys.conf
 cp /git/bootc/ops/sshd/hardening.conf /etc/ssh/sshd_config.d/10-hardening.conf
+cp /git/bootc/ops/sshd/hostkeys.conf /etc/ssh/sshd_config.d/30-hostkeys.conf
 cp /git/bootc/ops/sshd/authorizedkeys.pub /etc/ssh/authorized_keys/nathan
-
-ln --symbolic /var/ssh/ssh_host_ecdsa_key /etc/ssh/ssh_host_ecdsa_key
-ln --symbolic /var/ssh/ssh_host_ecdsa_key.pub /etc/ssh/ssh_host_ecdsa_key.pub
-# ln --symbolic /var/ssh/ssh_host_ed25519_key /etc/ssh/ssh_host_ed25519_key
-# ln --symbolic /var/ssh/ssh_host_ed25519_key.pub /etc/ssh/ssh_host_ed25519_key.pub
-# ln --symbolic /var/ssh/ssh_host_rsa_key /etc/ssh/ssh_host_rsa_key
-# ln --symbolic /var/ssh/ssh_host_rsa_key.pub /etc/ssh/ssh_host_rsa_key.pub
-
-chcon --no-dereference --type sshd_key_t /etc/ssh/ssh_host_ecdsa_key
-chcon --no-dereference --type sshd_key_t /etc/ssh/ssh_host_ecdsa_key.pub
-# chcon --no-dereference --type sshd_key_t /etc/ssh/ssh_host_ed25519_key
-# chcon --no-dereference --type sshd_key_t /etc/ssh/ssh_host_ed25519_key.pub
-# chcon --no-dereference --type sshd_key_t /etc/ssh/ssh_host_rsa_key
-# chcon --no-dereference --type sshd_key_t /etc/ssh/ssh_host_rsa_key.pub
 
 chmod u=rwx,go=rx /usr/bin/keygen
 
