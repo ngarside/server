06809e6c5753695b68109a670155e56364bb7537
Nathan Garside <git@nathangarside.com>
2026-05-10 11:09:34 +0100

    Run as exe

--- bootc/src/sshd.sh
+++ bootc/src/sshd.sh
@@ -3,14 +3,13 @@
 
 set -euo pipefail
 
-mkdir --parents /etc/keygen
 mkdir --parents /etc/ssh/authorized_keys
 mkdir --parents /etc/ssh/sshd_config.d
 
-cp /git/bootc/ops/sshd/keygen.sh /etc/keygen/keygen.sh
+cp /git/bootc/ops/sshd/keygen.sh /usr/bin/keygen
 cp /git/bootc/ops/sshd/keygen.service /usr/lib/systemd/system/keygen.service
 cp /git/bootc/ops/sshd/authorizedkeys.conf /etc/ssh/sshd_config.d/20-authorizedkeys.conf
 cp /git/bootc/ops/sshd/hardening.conf /etc/ssh/sshd_config.d/10-hardening.conf
 cp /git/bootc/ops/sshd/authorizedkeys.pub /etc/ssh/authorized_keys/nathan
 
-chmod ug=r,o= /etc/restic/restic.sh
+chmod u=rwx,go=rx /usr/bin/keygen
