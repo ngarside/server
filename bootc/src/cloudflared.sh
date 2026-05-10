8bd55b3db206f8595a2bef2753963db800ab858c
Nathan Garside <git@nathangarside.com>
2026-05-10 06:12:55 +0100

    Remove cloudflared (#2309)

--- bootc/src/cloudflared.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# This is free and unencumbered software released into the public domain.
-
-set -euo pipefail
-
-mkdir --parents /etc/containers/systemd/users/1001
-cp /git/cloudflared/ops/* /etc/containers/systemd/users/1001/
