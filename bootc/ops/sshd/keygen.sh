d01682ea766b9a16c7784c8fc6bee162d40329bc
Nathan Garside <git@nathangarside.com>
2026-07-04 08:59:56 +0100

    Update keygen.sh

--- bootc/ops/sshd/keygen.sh
+++ bootc/ops/sshd/keygen.sh
@@ -7,16 +7,32 @@ ROOT="${1:-$(pwd)}"
 
 mkdir --parents "$ROOT"
 
-if [ ! -f "$ROOT/ssh_host_ecdsa_key" ]; then
+# Generate ECDSA key if it doesn't already exist -------------------------------
+if [ -f "$ROOT/ssh_host_ecdsa_key" ]; then
+	echo "[SSHD Keygen] ECDSA key already exists - skipping"
+else
+	echo "[SSHD Keygen] ECDSA key not found - generating"
 	ssh-keygen -C '' -f "$ROOT/ssh_host_ecdsa_key" -P '' -t ecdsa
+	echo "[SSHD Keygen] ECDSA key generated"
 fi
 
-if [ ! -f "$ROOT/ssh_host_ed25519_key" ]; then
+# Generate ED25519 key if it doesn't already exist -----------------------------
+if [ -f "$ROOT/ssh_host_ed25519_key" ]; then
+	echo "[SSHD Keygen] ED25519 key already exists - skipping"
+else
+	echo "[SSHD Keygen] ED25519 key not found - generating"
 	ssh-keygen -C '' -f "$ROOT/ssh_host_ed25519_key" -P '' -t ed25519
+	echo "[SSHD Keygen] ED25519 key generated"
 fi
 
-if [ ! -f "$ROOT/ssh_host_rsa_key" ]; then
+# Generate RSA key if it doesn't already exist ---------------------------------
+if [ -f "$ROOT/ssh_host_rsa_key" ]; then
+	echo "[SSHD Keygen] RSA key already exists - skipping"
+else
+	echo "[SSHD Keygen] RSA key not found - generating"
 	ssh-keygen -b 4096 -C '' -f "$ROOT/ssh_host_rsa_key" -P '' -t rsa
+	echo "[SSHD Keygen] RSA key generated"
 fi
 
+# Ensure all generated keys are labelled correctly -----------------------------
 chcon --type sshd_key_t "$ROOT/*"
